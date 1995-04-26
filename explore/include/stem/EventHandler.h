e471bc9fd36eb3cbda26616ad5357ae516fa141b
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1995-04-26 17:51:57 +0000

    Initial revision
    git-svn-id: svn://localhost/complement/trunk@2 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/stem/EventHandler.h
@@ -0,0 +1,131 @@
+#ifndef __OXW_EventHandler_h // -*- C++ -*-
+#define __OXW_EventHandler_h
+
+#ifndef _XLIB_H_
+#include <X11/Xlib.h>
+#endif
+
+#ifndef __OXW_OXWdefs_h
+#include <OXW/OXWdefs.h>
+#endif
+
+#ifndef __OXW_Event_h
+#include <OXW/Event.h>
+#endif
+
+
+class GENERIC;
+template <class T> class ASResponseTableEntry;  // forward declaration
+typedef ASResponseTableEntry<GENERIC> ASGenericTableEntry;
+
+class ASEventHandler
+{
+  public:
+    class ASEventInfo
+    {
+      public:
+        ASEventInfo( unsigned long msg, unsigned long id = 0) : 
+	  Msg(msg), Id(id), Entry(0) {}
+
+        const unsigned long Msg;
+        const unsigned long Id;
+        GENERIC *Object;
+        ASGenericTableEntry *Entry;
+    };
+    typedef int(*ASEqualOperator)( ASGenericTableEntry&, ASEventInfo& );
+
+    // Find object corresponded to event info, return non-zero in success
+    // end zero otherwise. You may make influence to selection of
+    // event processor by specifying equal operator.
+    virtual int Find( ASEventInfo&, ASEqualOperator = 0 );
+
+    // Dispatch event info (with message as parameter)
+    virtual long Dispatch( ASEventInfo&, ASEvent& );
+
+  protected:
+    int SearchEntries( ASGenericTableEntry *, ASEventInfo&, ASEqualOperator );
+};
+
+template <class T> class ASResponseTableEntry
+{
+  public:
+    typedef void (T::*PMF)( ASEvent& );
+
+    union {
+      unsigned long Msg;
+      unsigned long NotifyCode;
+    };
+    unsigned long Id;
+    PMF Pmf;
+};
+
+//
+// macros to declare a response table
+//
+
+#define DECLARE_RESPONSE_TABLE(cls)\
+  private:\
+    static ASResponseTableEntry< cls > __entries[];\
+    typedef cls ThisCls;\
+  public:\
+    int  Find( ASEventInfo&, ASEqualOperator = 0 )
+
+#define END_RESPONSE_TABLE\
+  {0, 0, 0}}
+
+#define DEFINE_RESPONSE_TABLE_ENTRIES(cls)\
+  ASResponseTableEntry< cls > cls::__entries[] = {
+
+//
+// macro to define a response table for a class with no base response tables
+//
+// you use it like this:
+//    DEFINE_RESPONSE_TABLE(cls)
+//      EV_WM_PAINT,
+//      EV_WM_LBUTTONDOWN,
+//    END_RESPONSE_TABLE;
+//
+#define DEFINE_RESPONSE_TABLE(cls)\
+  int cls::Find(ASEventInfo& eventInfo, ASEqualOperator equal){\
+       eventInfo.Object = (GENERIC *)this;\
+       return SearchEntries((ASGenericTableEntry *)__entries, eventInfo, equal);}\
+  DEFINE_RESPONSE_TABLE_ENTRIES(cls)
+
+//
+// macro to define a response table for a class with one base. use this macro
+// exactly like macro DEFINE_RESPONSE_TABLE
+//
+#define DEFINE_RESPONSE_TABLE1(cls, base)\
+  int cls::Find(ASEventInfo& eventInfo, ASEqualOperator equal){\
+       eventInfo.Object = (GENERIC *)this;\
+       return SearchEntries((ASGenericTableEntry *)__entries, eventInfo, equal) ||\
+              base::Find(eventInfo, equal);}\
+  DEFINE_RESPONSE_TABLE_ENTRIES(cls)
+
+//
+// macro to define a response table for a class with two bases. use this macro
+// exactly like macro DEFINE_RESPONSE_TABLE
+//
+#define DEFINE_RESPONSE_TABLE2(cls, base1, base2)\
+  int cls::Find(ASEventInfo& eventInfo, ASEqualOperator equal){\
+       eventInfo.Object = (GENERIC *)this;\
+       return SearchEntries((ASGenericTableEntry *)__entries, eventInfo, equal) ||\
+              base1::Find(eventInfo, equal) ||\
+              base2::Find(eventInfo, equal);}\
+  DEFINE_RESPONSE_TABLE_ENTRIES(cls)
+
+//
+// macro to define a response table for a class with three bases. use this macro
+// exactly like macro DEFINE_RESPONSE_TABLE
+//
+#define DEFINE_RESPONSE_TABLE3(cls, base1, base2, base3)\
+  int cls::Find(ASEventInfo& eventInfo, ASEqualOperator equal)\
+       eventInfo.Object = (GENERIC *)this;\
+       return SearchEntries((ASGenericTableEntry *)__entries, eventInfo, equal) ||\
+              base1::Find(eventInfo, equal) ||\
+              base2::Find(eventInfo, equal) ||\
+              base3::Find(eventInfo, equal);}\
+  DEFINE_RESPONSE_TABLE_ENTRIES(cls)
+
+
+#endif          // __OXW_EventHandler_h
---
+++ explore/lib/stem/_EventHandler.cc
@@ -0,0 +1,41 @@
+#include <OXW/EventHandler.h>
+#include <CLASSLIB/checks.h>
+
+int ASEventHandler::Find( ASEventInfo&, ASEqualOperator )
+{
+  return 0;
+}
+
+long ASEventDispatch(GENERIC& generic, void (GENERIC::*pmf)( ASEvent& ), ASEvent& event )
+{
+  (generic.*pmf)( event );
+  return 0;
+}
+
+long ASEventHandler::Dispatch( ASEventInfo& eventInfo, ASEvent& event )
+{
+  PRECONDITION(eventInfo.Entry);
+  return ASEventDispatch( *eventInfo.Object, eventInfo.Entry->Pmf, event );
+}
+
+int ASEventHandler::SearchEntries( ASGenericTableEntry *entries, ASEventInfo& eventInfo, ASEqualOperator equal )
+{
+  if ( equal ) {
+    while ( entries->Pmf != 0 ) {
+      if ( equal( *entries, eventInfo ) ) {
+        eventInfo.Entry = entries;
+        return 1;
+      }
+      ++entries;
+    }
+  } else {
+    while ( entries->Pmf != 0 ) {
+      if ( entries->Msg == eventInfo.Msg && entries->Id == eventInfo.Id ) {
+        eventInfo.Entry = entries;
+        return 1;
+      }
+      ++entries;
+    }
+  }
+  return 0;
+}
