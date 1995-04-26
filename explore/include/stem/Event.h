874a1a7bd25cea8df35c63bf6912fcf68fa67525
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1995-04-26 17:51:57 +0000

    Initial revision
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@2 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/stem/Event.h
@@ -0,0 +1,212 @@
+#ifndef __OXW_Event_h // -*- C++ -*-
+#define __OXW_Event_h
+
+#ifndef _XLIB_H_
+#include <X11/Xlib.h>
+#endif
+
+#ifndef __OXW_OXWdefs_h
+#include <OXW/OXWdefs.h>
+#endif
+
+#ifndef __CLASSLIB_checks_h
+#include <CLASSLIB/checks.h>
+#endif
+
+struct tagLParam {
+  unsigned char Hi;
+  unsigned char hi;
+  unsigned char lo;
+  unsigned char Lo;
+};
+
+struct tagIParams
+{
+  union {
+    unsigned long AParam;
+    tagLParam AP;
+  };
+  union {
+    unsigned long BParam;
+    tagLParam BP;
+  };
+  union {
+    unsigned long CParam;
+    tagLParam CP;
+  };
+  union {
+    unsigned long DParam;
+    tagLParam DP;
+  };
+};
+
+struct tagFParams
+{
+  double XParam;
+  double YParam;
+};
+
+class ASEvent
+{
+  public:
+    enum contained_type {
+      IntegerParams,
+      FloatParams,
+      StringParam,
+      XEventParam
+    };
+
+    unsigned long Message;
+
+    ASEvent( unsigned long );
+    ASEvent( unsigned long, const tagIParams& );
+    ASEvent( unsigned long, const tagFParams& );
+    ASEvent( unsigned long, const char * );
+    ASEvent( unsigned long, XEvent& );
+    ASEvent( XEvent& );
+
+    void SetMessage( unsigned long );
+    void SetIParams( unsigned long, const tagIParams& );
+    void SetFParams( unsigned long, const tagFParams& );
+    void SetSParam( unsigned long, const char * );
+    void SetXEvent( unsigned long, XEvent& );
+    void SetXEvent( XEvent& );
+
+    unsigned long GetMessage() { return Message; }
+    tagIParams& GetIParams();
+    tagFParams& GetFParams();
+    char *GetSParam();
+    XEvent *GetXEvent();
+
+    contained_type isA();
+    int isA( contained_type );
+
+  private:
+    union {
+      tagIParams IParams;
+      tagFParams FParams;
+      char SParam[16];
+      XEvent *Xevent;
+    };
+    contained_type type;
+};
+
+inline ASEvent::ASEvent( unsigned long msg )
+{
+  Message = msg;
+  type = IntegerParams;
+  IParams.AParam = 0;
+  IParams.BParam = 0;
+  IParams.CParam = 0;
+  IParams.DParam = 0;
+}
+
+inline ASEvent::ASEvent( unsigned long msg, const tagIParams& ip )
+{
+  Message = msg;
+  type = IntegerParams;
+  IParams.AParam = ip.AParam;
+  IParams.BParam = ip.BParam;
+  IParams.CParam = ip.CParam;
+  IParams.DParam = ip.DParam;
+}
+
+inline ASEvent::ASEvent( unsigned long msg, const tagFParams& fp )
+{
+  Message = msg;
+  type = FloatParams;
+  FParams.XParam = fp.XParam;
+  FParams.YParam = fp.YParam;
+}
+
+inline ASEvent::ASEvent( unsigned long msg, XEvent& event )
+{
+  Message = msg;
+  type = XEventParam;
+  Xevent = &event;
+}
+
+inline ASEvent::ASEvent( XEvent& event )
+{
+  Message = event.type;
+  type = XEventParam;
+  Xevent = &event;
+}
+
+inline void ASEvent::SetMessage( unsigned long msg )
+{
+  Message = msg;
+  type = IntegerParams;
+  IParams.AParam = 0;
+  IParams.BParam = 0;
+  IParams.CParam = 0;
+  IParams.DParam = 0;
+}
+
+inline void ASEvent::SetIParams( unsigned long msg, const tagIParams& ip )
+{
+  Message = msg;
+  type = IntegerParams;
+  IParams.AParam = ip.AParam;
+  IParams.BParam = ip.BParam;
+  IParams.CParam = ip.CParam;
+  IParams.DParam = ip.DParam;
+}
+
+inline void ASEvent::SetFParams( unsigned long msg, const tagFParams& fp )
+{
+  Message = msg;
+  type = FloatParams;
+  FParams.XParam = fp.XParam;
+  FParams.YParam = fp.YParam;
+}
+
+inline void ASEvent::SetXEvent( unsigned long msg, XEvent& event )
+{
+  Message = msg;
+  type = XEventParam;
+  Xevent = &event;
+}
+
+inline void ASEvent::SetXEvent( XEvent& event )
+{
+  Message = event.type;
+  type = XEventParam;
+  Xevent = &event;
+}
+
+inline tagIParams& ASEvent::GetIParams()
+{
+  PRECONDITION( type == IntegerParams );
+  return IParams;
+}
+
+inline tagFParams& ASEvent::GetFParams()
+{
+  PRECONDITION( type == FloatParams );
+  return FParams;
+}
+
+inline char *ASEvent::GetSParam()
+{
+  PRECONDITION( type == StringParam );
+  return SParam;
+}
+
+inline XEvent *ASEvent::GetXEvent()
+{
+  PRECONDITION( type == XEventParam );
+  return Xevent;
+}
+
+inline ASEvent::contained_type ASEvent::isA()
+{
+  return type;
+}
+
+inline int ASEvent::isA( ASEvent::contained_type tstType )
+{
+  return tstType == type;
+}
+
+#endif
