4ba812eeebaef2479d99a035003851a0c8ce198d
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1995-11-11 18:51:47 +0000

    Work now (stack of object's states)
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@103 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/_EventHandler.cc
+++ explore/lib/stem/_EventHandler.cc
@@ -1,52 +1,73 @@
 #ident "%Z%%Q% $RCSfile$ v$Revision$ %H% %T%"
 
 #include <OXW/EventHandler.h>
+#include <OXW/OXWEvents.h>
 #include <CLASS/checks.h>
 
-int OXWEventHandler::Find( OXWEventInfo&, OXWEqualOperator )
+#include <strstream.h>
+
+GENERIC_StatesTbl OXWEventHandler::theStatesTable = OXWEventHandler::RTConfigure();
+int OXWEventHandler::ini_flag = 0;
+
+GENERIC_StatesTbl OXWEventHandler::RTConfigure()
 {
-  return 0;
+  GENERIC_StatesTbl tmp;
+  return tmp;
 }
 
-class GENERIC
-{
-  virtual void pure( OXWEvent& ) = 0; // This is to allow usage of virtual
-	                              // catchers, indeed never used.
-};
 
-typedef void (GENERIC::*GENERIC_PMF)( OXWEvent& );
-
-inline
-long OXWEventDispatch( GENERIC& generic, GENERIC_PMF pmf, OXWEvent& event )
+void OXWEventHandler::PopState()
 {
-  (generic.*pmf)( event );
-  return 0;
-}
-
-long OXWEventHandler::Dispatch( OXWEventInfo& eventInfo, OXWEvent& event )
-{
-  PRECONDITION(eventInfo.Entry);
-  return OXWEventDispatch( *(eventInfo.Object), eventInfo.Entry->Pmf, event );
-}
-
-int OXWEventHandler::SearchEntries( OXWGenericTableEntry *entries, OXWEventInfo& eventInfo, OXWEqualOperator equal )
-{
-  if ( equal ) {
-    while ( entries->Pmf != 0 ) {
-      if ( equal( *entries, eventInfo ) ) {
-        eventInfo.Entry = entries;
-        return 1;
-      }
-      ++entries;
-    }
-  } else {
-    while ( entries->Pmf != 0 ) {
-      if ( entries->Msg == eventInfo.Msg && entries->Id == eventInfo.Id ) {
-        eventInfo.Entry = entries;
-        return 1;
-      }
-      ++entries;
-    }
+  theHistory.pop_back();
+  while ( theHistory.back() == ST_TERMINAL ) {
+    theHistory.pop_back();
   }
-  return 0;
+}
+
+OXWEventHandler::OXWEventHandler()
+{
+  State( ST_NULL );
+}
+
+bool OXWEventHandler::__Dispatch( state_type state, OXWEvent& event )
+{
+  TRACE( "Dispatch " << isA() << "\n" <<  theStatesTable.PrintContents() );
+  return theStatesTable.Dispatch( *((GENERIC *)this), state, event );
+}
+
+bool OXWEventHandler::Dispatch( OXWEvent& event )
+{
+  {
+    list<state_type>::reverse_iterator hst_i = theHistory.rbegin();
+    ostrstream buf;
+    buf << "State stack (" << isA() << "):\n";
+    while ( hst_i != theHistory.rend() ) {
+      buf << "\tstate: " << *hst_i << "\n";
+      ++hst_i;
+    }
+    buf << ends;
+    TRACE( buf.str() );
+  }
+  list<state_type>::reverse_iterator hst_i = theHistory.rbegin();
+  bool d = 0;
+  while ( hst_i != theHistory.rend() && *hst_i != ST_TERMINAL && 
+          !(d =__Dispatch( *hst_i, event ) ) ) {
+    ++hst_i;
+  }
+  if ( !d && State() == ST_NULL ) {
+    TRACE( "Sorry, init now...(" << isA() << ")" );
+    OXWEvRTInit( OXWEvent( OXW_RTINIT ) );
+    d = Dispatch( event );
+  }
+  return d;
+}
+
+void OXWEventHandler::OXWEvRTInit( OXWEvent& )
+{
+  OXWRTInit();
+  State( ST_READY );
+}
+
+void OXWEventHandler::OXWRTInit()
+{ // do nothing: this is stub
 }
