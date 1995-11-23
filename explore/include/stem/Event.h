41835cb3c2c0621d31f7063e4c811cb892125a36
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1995-11-23 11:55:41 +0000

    Fully rewritten: another consept used.
    git-svn-id: svn://localhost/complement/trunk@107 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/include/stem/Event.h
+++ explore/include/stem/Event.h
@@ -1,4 +1,4 @@
-#ident "%Z%$RCSfile$ v.$Revision$ %H% %T%" // -*- C++ -*-
+#ident "%Z%%Q%$RCSfile$ ($Revision$): %H% %T%" // -*- C++ -*-
 
 #ifndef __OXW_Event_h
 #define __OXW_Event_h
@@ -15,197 +15,72 @@
 #include <CLASS/checks.h>
 #endif
 
-struct tagLParam {
-  unsigned char Hi;
-  unsigned char hi;
-  unsigned char lo;
-  unsigned char Lo;
+class GENERIC;
+template <class T> class OXWEventsTable<T>;
+class OXWEventsCore;
+template <class S, class D > class OXWEventT;
+typedef OXWEventT<OXWEventsCore,GENERIC> OXWEvent;
+
+struct OXWInt2
+{
+  int A;
+  int B;
 };
 
-struct tagIParams
+template <class S, class D >
+class OXWEventT
 {
-  union {
-    unsigned long AParam;
-    tagLParam AP;
-  };
-  union {
-    unsigned long BParam;
-    tagLParam BP;
-  };
-  union {
-    unsigned long CParam;
-    tagLParam CP;
-  };
-  union {
-    unsigned long DParam;
-    tagLParam DP;
-    class OXWEventsCore *ECObject;
-    class OXWWindowBase *WBObject;
-  };
-};
+  friend OXWEventsTable<GENERIC>;
 
-struct tagFParams
-{
-  double XParam;
-  double YParam;
-};
-
-class OXWEvent
-{
   public:
-    enum contained_type {
-      IntegerParams,
-      FloatParams,
-      StringParam,
-      XEventParam
-    };
+    OXWEventT() :
+	Message( 0 ),
+	theSender( 0 ),
+	DataObject( 0 )
+      { }
+    OXWEventT( message_type msg ) :
+	Message( msg ),
+	theSender( 0 ),
+	DataObject( 0 )
+      { }
+    OXWEventT( message_type msg, D *data ) :
+	Message( msg ),
+	theSender( 0 ),
+	DataObject( data )
+      { }
+    OXWEventT( XEvent& x ) :
+	Message( x.type ),
+	theSender( 0 ),
+	DataObject( (D *)&x )
+      { }
+    OXWEventT( OXWEvent& e ) :
+	Message( e.GetMessage() ),
+	theSender( (S *)e.Sender() ),
+	DataObject( (D *)e.Data() )
+      { }
 
-    unsigned long Message;
-
-    OXWEvent();
-    OXWEvent( unsigned long );
-    OXWEvent( unsigned long, const tagIParams& );
-    OXWEvent( unsigned long, const tagFParams& );
-    OXWEvent( unsigned long, const char * );
-    OXWEvent( unsigned long, XEvent& );
-    OXWEvent( XEvent& );
-
-    void SetMessage( unsigned long );
-    void SetIParams( unsigned long, const tagIParams& );
-    void SetFParams( unsigned long, const tagFParams& );
-    void SetSParam( unsigned long, const char * );
-    void SetXEvent( unsigned long, XEvent& );
-    void SetXEvent( XEvent& );
-
-    unsigned long GetMessage() const
+    message_type GetMessage() const
       { return Message; }
-    tagIParams& GetIParams();
-    tagFParams& GetFParams();
-    char *GetSParam();
-    XEvent *GetXEvent();
-
-    contained_type isA() const;
-    int isA( contained_type ) const;
+    S *Sender() const
+      { return theSender; }
+    D *Data() const
+      { return DataObject; }
+    void Sender( S *x )
+      { theSender = x; }
+    operator OXWEvent& ()
+      { return *( (OXWEventT<OXWEventsCore,GENERIC> *)this ); }
+    operator XEvent& ()
+      {
+	PRECONDITION( Message < LASTEvent );
+	return *((XEvent *)DataObject);
+      }
+    operator OXWInt2& ()
+      { return *((OXWInt2 *)DataObject); }
 
   private:
-    union {
-      tagIParams IParams;
-      tagFParams FParams;
-      char SParam[16];
-      XEvent *Xevent;
-    };
-    contained_type type;
+    message_type Message;
+    S *theSender;
+    D *DataObject;
 };
-
-inline OXWEvent::OXWEvent()
-{
-  memset( this, 0, sizeof( OXWEvent ) );
-}
-
-inline OXWEvent::OXWEvent( unsigned long msg )
-{
-  Message = msg;
-  type = IntegerParams;
-  memset( &IParams, 0, sizeof( unsigned long ) * 4 );
-}
-
-inline OXWEvent::OXWEvent( unsigned long msg, const tagIParams& ip )
-{
-  Message = msg;
-  type = IntegerParams;
-  memcpy( &IParams, &ip, sizeof( unsigned long ) * 4 );
-}
-
-inline OXWEvent::OXWEvent( unsigned long msg, const tagFParams& fp )
-{
-  Message = msg;
-  type = FloatParams;
-  FParams.XParam = fp.XParam;
-  FParams.YParam = fp.YParam;
-}
-
-inline OXWEvent::OXWEvent( unsigned long msg, XEvent& event )
-{
-  Message = msg;
-  type = XEventParam;
-  Xevent = &event;
-}
-
-inline OXWEvent::OXWEvent( XEvent& event )
-{
-  Message = event.type;
-  type = XEventParam;
-  Xevent = &event;
-}
-
-inline void OXWEvent::SetMessage( unsigned long msg )
-{
-  Message = msg;
-  type = IntegerParams;
-  memset( &IParams, 0, sizeof( unsigned long ) * 4 );
-}
-
-inline void OXWEvent::SetIParams( unsigned long msg, const tagIParams& ip )
-{
-  Message = msg;
-  type = IntegerParams;
-  memcpy( &IParams, &ip, sizeof( unsigned long ) * 4 );
-}
-
-inline void OXWEvent::SetFParams( unsigned long msg, const tagFParams& fp )
-{
-  Message = msg;
-  type = FloatParams;
-  FParams.XParam = fp.XParam;
-  FParams.YParam = fp.YParam;
-}
-
-inline void OXWEvent::SetXEvent( unsigned long msg, XEvent& event )
-{
-  Message = msg;
-  type = XEventParam;
-  Xevent = &event;
-}
-
-inline void OXWEvent::SetXEvent( XEvent& event )
-{
-  Message = event.type;
-  type = XEventParam;
-  Xevent = &event;
-}
-
-inline tagIParams& OXWEvent::GetIParams()
-{
-  PRECONDITION( type == IntegerParams );
-  return IParams;
-}
-
-inline tagFParams& OXWEvent::GetFParams()
-{
-  PRECONDITION( type == FloatParams );
-  return FParams;
-}
-
-inline char *OXWEvent::GetSParam()
-{
-  PRECONDITION( type == StringParam );
-  return SParam;
-}
-
-inline XEvent *OXWEvent::GetXEvent()
-{
-  PRECONDITION( type == XEventParam );
-  return Xevent;
-}
-
-inline OXWEvent::contained_type OXWEvent::isA() const
-{
-  return type;
-}
-
-inline int OXWEvent::isA( OXWEvent::contained_type tstType ) const
-{
-  return tstType == type;
-}
 
 #endif
