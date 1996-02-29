f174b5ac73c1ef81eaef08638c1b242ad43c0e11
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1996-02-29 11:56:13 +0000

    Radical revision of events -- states container.
    git-svn-id: svn://localhost/complement/trunk@118 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/_EventHandler.cc
+++ explore/lib/stem/_EventHandler.cc
@@ -1,21 +1,42 @@
-#ident "%Z%%Q%$RCSfile$ ($Revision$): %H% %T%"
+// -*- C++ -*- Time-stamp: <96/02/28 11:38:24 ptr>
+#ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
 
 #include <OXW/EventHandler.h>
 #include <OXW/OXWEvents.h>
 #include <CLASS/checks.h>
 
 // #include <iostream.h>
+#if defined(__TRACE) || defined(__WARN)
 
-char trace_buffer[4096];
+DIAG_DEFINE_GROUP(EventHandler,0,4);
+DIAG_DEFINE_GROUP(EventDispatch,0,3);
 
-ostrstream OXWEventHandler::Out( trace_buffer, 4096 );
-GENERIC_StatesTbl OXWEventHandler::theStatesTable;
+#endif // __TRACE || __WARN
+
+OXWEventHandler::evtable_type OXWEventHandler::theEventsTable(
+  "OXWEventHandler", OXWEventHandler::theDeclEventsTable );
+
+__DeclareAnyPMF<OXWEventHandler> OXWEventHandler::theDeclEventsTable[] = {
+  { 0, 0, { 0, 0, "End of table" }}
+};
 
 DEFINE_NAME_IT( OXWEventHandler );
 
-int OXWEventHandler::RespTblConfigure()
+void OXWEventHandler::PushState( state_type state )
 {
-  return 1;
+  RemoveState( state );
+  theHistory.push_back( state );
+}
+
+state_type OXWEventHandler::State() const
+{
+  return theHistory.back();
+}
+
+void OXWEventHandler::PushTState( state_type state )
+{
+  theHistory.push_back( ST_TERMINAL );
+  theHistory.push_back( state );
 }
 
 void OXWEventHandler::PopState()
@@ -28,7 +49,7 @@ void OXWEventHandler::PopState()
 
 void OXWEventHandler::PopState( state_type state )
 {
-  list<state_type>::iterator hst_i = __find( state );
+  h_iterator hst_i = __find( state );
   if ( hst_i != theHistory.end() && *hst_i != ST_TERMINAL ) {
     theHistory.erase( hst_i, theHistory.end() );
   }
@@ -36,7 +57,7 @@ void OXWEventHandler::PopState( state_type state )
 
 void OXWEventHandler::RemoveState( state_type state )
 {
-  list<state_type>::iterator hst_i = __find( state );
+  h_iterator hst_i = __find( state );
   if ( hst_i != theHistory.end() && *hst_i != ST_TERMINAL ) {
     theHistory.erase( hst_i );
   }
@@ -44,23 +65,23 @@ void OXWEventHandler::RemoveState( state_type state )
 
 bool OXWEventHandler::isState( state_type state ) const
 {
-  list<state_type>::const_reverse_iterator hst_i = theHistory.rbegin();
+  const HistoryContainer& hst = theHistory;
+  HistoryContainer::const_reverse_iterator hst_i = hst.rbegin();
 
-  while ( hst_i != theHistory.rend() && *hst_i != ST_TERMINAL ) {
-    if ( *hst_i == state ) {
+  while ( hst_i != hst.rend() && *hst_i != ST_TERMINAL ) {
+    if ( *hst_i++ == state ) {
       return true;
     }
-    ++hst_i;
   }
   return false;
 }
 
-list<state_type>::iterator OXWEventHandler::__find( state_type state )
+h_iterator OXWEventHandler::__find( state_type state )
 {
   if ( theHistory.empty() ) {
     return theHistory.end();
   }
-  list<state_type>::iterator hst_i = theHistory.end();
+  h_iterator hst_i = theHistory.end();
 
   while ( --hst_i != theHistory.begin() && *hst_i != ST_TERMINAL ) {
     if ( *hst_i == state ) {
@@ -73,44 +94,46 @@ list<state_type>::iterator OXWEventHandler::__find( state_type state )
   return hst_i;
 }
 
-OXWEventHandler::OXWEventHandler()
+// Trick with reference to theHistory instead of object itself is here due to
+// SunPro's C++ 4.1 bug (its occur only for large enough programm):
+// instances of theHistory in inline and outline methods are not same!!!
+// Of cause, you can found this only if theHistory is a template with static
+// member (stl/list, for example).
+
+OXWEventHandler::OXWEventHandler() :
+    theHistory( *(new HistoryContainer()) )
 {
   State( ST_NULL );
 }
 
-bool OXWEventHandler::__Dispatch( state_type state, OXWEvent& event )
+OXWEventHandler::~OXWEventHandler()
 {
-  return theStatesTable.Dispatch( (GENERIC *)this, state, event );
+  delete &theHistory;
 }
 
 bool OXWEventHandler::Dispatch( OXWEvent& event )
 {
-//  cerr <<  "........\n" << Trace() << endl;
-  list<state_type>::reverse_iterator hst_i = theHistory.rbegin();
-  bool d = false;
-  while ( hst_i != theHistory.rend() && *hst_i != ST_TERMINAL && 
-          !(d =__Dispatch( *hst_i, event ) ) ) {
-    ++hst_i;
-  }
-  return d;
+  return theEventsTable.Dispatch( this, theHistory.begin(),
+				  theHistory.end(), event );
+}
+
+void OXWEventHandler::DispatchTrace( OXWEvent& __event__, ostrstream& out )
+{
+  theEventsTable.DispatchTrace( theHistory.begin(),
+	 		        theHistory.end(), __event__, out );
 }
 
 void OXWEventHandler::TraceStack( ostrstream& out ) const
 {
-  list<state_type>::const_reverse_iterator hst_i = theHistory.rbegin();
-  out << "State stack (" << isA() << "):\n";
-  while ( hst_i != theHistory.rend() ) {
-    out << "\tstate: " << *hst_i << "\n";
-    ++hst_i;
+  const HistoryContainer& hst = theHistory;
+  HistoryContainer::const_reverse_iterator hst_i = hst.rbegin();
+  while ( hst_i != hst.rend() ) {
+    out << "[" << *hst_i++ << "]";
   }
+  out << endl;
 }
 
-const char *OXWEventHandler::Trace() const
+void OXWEventHandler::Trace( ostrstream& out ) const
 {
-  Out.seekp( 0, ostream::beg );
-  TraceStack( Out );
-  OXWEventHandler::theStatesTable.Out( Out );
-  Out << ends;
-
-  return Out.str();
+  OXWEventHandler::theEventsTable.Out( out );
 }
