1371568cfd2e801341d0a250e1414a743458a33d
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1996-10-10 14:29:25 +0000

    Names changed.
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@141 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/EvManager.cc
+++ explore/lib/stem/EvManager.cc
@@ -1,36 +1,36 @@
-// -*- C++ -*- Time-stamp: <96/10/07 19:28:48 ptr>
+// -*- C++ -*- Time-stamp: <96/10/09 18:34:54 ptr>
 #ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
 
 #include <EDS/EvManager.h>
 #include <EDS/EventsCore.h>
 
-void OXWEvManager::dispatch()
+void EDSEvManager::dispatch()
 {
   queue_lock.wait();
   while ( !empty() ) {
     // queue_sem.wait();
     OXWEvInfo& top = front();
     switch ( top.first.dtype ) {
-      case OXWEvTarget::Local:
+      case EDSEvTarget::Local:
 	switch ( top.first.ctype ) {
-	  case OXWEvTarget::This:
+	  case EDSEvTarget::This:
 	    top.first.target->Notify( top.second );
 	    break;
-	  case OXWEvTarget::BranchChilds:
+	  case EDSEvTarget::BranchChilds:
 	    top.first.target->BranchNotifyChilds( top.second );
 	    break;
-	  case OXWEvTarget::Branch:
+	  case EDSEvTarget::Branch:
 	    top.first.target->BranchNotify( top.second );
 	    break;
-	  case OXWEvTarget::Siblings:
+	  case EDSEvTarget::Siblings:
 	    top.first.target->SiblingNotify( top.second );
 	    break;
-	  case OXWEvTarget::Parent:
+	  case EDSEvTarget::Parent:
 	    top.first.target->ParentNotify( top.second );
 	    break;
 	}
 	break;
-      case OXWEvTarget::Global:
+      case EDSEvTarget::Global:
 	WARN( true, "Global events not accepted yet" );
 	break;
     }
@@ -42,12 +42,12 @@ void OXWEvManager::dispatch()
   queue_lock.set_condition();
 }
 
-void OXWEvManager::Done()
+void EDSEvManager::Done()
 {
   lock_not_done.signal();
 }
 
-bool OXWEvManager::wait_done()
+bool EDSEvManager::wait_done()
 {
   lock_not_done.wait();
 
