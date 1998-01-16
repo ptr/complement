91f6d499f360de54ffce83f46aa2ab78caaea7a2
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1998-01-16 19:15:37 +0000

    Initial revision
    git-svn-id: svn://localhost/complement/trunk@158 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/misc/directory.h
@@ -0,0 +1,74 @@
+// <!!-*-C++-*- file: directory.h --->
+// <!!-------------------------------------------------------------------------->
+// <!! Copyright (C) 1997 Dietmar Kuehl >
+// <!!   Universitaet Konstanz, Germany, Lehrstuhl fuer praktische Informatik I >
+// <!!>
+// <!! Permission to use, copy, modify, distribute and sell this >
+// <!! software for any purpose is hereby granted without fee, provided >
+// <!! that the above copyright notice appears in all copies and that >
+// <!! both that copyright notice and this permission notice appear in >
+// <!! supporting documentation. The Universitaet Konstanz and the >
+// <!! Lehrstuhl fuer praktische Informatik I make no representations >
+// <!! about the suitability of this software for any purpose. It is >
+// <!! provided "as is" without express or implied warranty. >
+// <!!-------------------------------------------------------------------------->
+
+// Author: Dietmar Kühl dietmar.kuehl@uni-konstanz.de www.informatik.uni-konstanz.de/~kuehl
+// Title:  Declaration for an iterator traversing the entries of a directory
+
+//------------------------------------------------------------------------------
+
+#if !defined(DIRECTORY_H)
+#define DIRECTORY_H
+
+#ifndef __SGI_STL_CONFIG_H
+#include <stl_config.h>
+#endif
+
+#ifndef __STRING__
+#include <string>
+#endif
+
+#ifndef __SGI_STL_ITERATOR
+#include <iterator>
+#endif
+
+// -----------------------------------------------------------------------------
+// The basic building block: An input iterator used to iterate over
+// the entries of a directory. This iterator only provides access to
+// the names of the entries which are returned from code(operator*)()
+// as code(string)s.
+
+class dir_it_rep;
+
+class dir_it :
+    public input_iterator<string, int>
+{
+  public:
+    // --------------------------------------------------------------------
+    // default ctor: creates an iterator suitable as past the end indicator
+    dir_it() :
+	i_rep(0)
+      { }
+    explicit dir_it(string const &);
+    dir_it(dir_it const &it);
+    ~dir_it();
+
+    dir_it &operator= (dir_it const &it);
+
+    string operator* () const { return i_value; }
+
+    dir_it &operator++ ();
+    dir_it operator++ (int) { dir_it rc (*this); operator++(); return rc; }
+
+    bool operator== (dir_it const &it) const;
+    bool operator!= (dir_it const &it) const { return !operator== (it); }
+
+  private:
+    dir_it_rep *i_rep;    // representation for the next value
+    string     i_value;   // the current value
+};
+
+//--------------------------------------------------------------------------
+
+#endif /* DIRECTORY_H */
