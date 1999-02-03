b576c690aa3b32366bb88996a83ee499e2083a1b
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-02-03 16:57:20 +0000

    Some funcs move here from header.
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@169 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/include/sockios/sockstream.cc
+++ explore/include/sockios/sockstream.cc
@@ -1,8 +1,115 @@
-// -*- C++ -*- Time-stamp: <99/02/02 18:04:58 ptr>
+// -*- C++ -*- Time-stamp: <99/02/03 15:54:17 ptr>
 
 #ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
 
 namespace std {
+
+template<class charT, class traits>
+basic_sockbuf<charT, traits> *
+basic_sockbuf<charT, traits>::open( const char *hostname, int port,
+				    sock_base::stype type = sock_base::sock_stream,
+				    sock_base::protocol prot = sock_base::inet )
+{
+  if ( is_open() ) {
+    return 0;
+  }
+  _mode = ios_base::in | ios_base::out;
+  _state = ios_base::goodbit;
+  _errno = 0;
+#ifdef WIN32
+  WSASetLastError( 0 );
+#endif
+  if ( prot == sock_base::inet ) {
+    _fd = socket( PF_INET, type, 0 );
+    if ( _fd == -1 ) {
+      _state |= sock_base::sockfailbit;
+#ifdef WIN32
+      _errno = WSAGetLastError();
+#else
+      _errno = ::errno;
+#endif
+      return 0;
+    }
+    _address.inet.sin_family = AF_INET;
+    _address.inet.sin_port = htons( port );
+    findhost( hostname );
+    if ( _state != ios_base::goodbit ) {
+#ifdef WIN32
+      ::closesocket( _fd );
+#else
+      ::close( _fd );
+#endif
+      return 0;
+    }
+	  
+    // Generally, stream sockets may successfully connect() only once
+    if ( connect( _fd, &_address.any, sizeof( _address ) ) == -1 ) {
+      _state |= sock_base::connfailbit;
+#ifdef WIN32
+      _errno = WSAGetLastError();
+      ::closesocket( _fd );
+#else
+      _errno = ::errno;
+      ::close( _fd );
+#endif
+      return 0;
+    }
+    if ( type == sock_base::sock_stream ) {
+      _xwrite = write;
+      _xread = read;
+    } else if ( type == sock_base::sock_dgram ) {
+      _xwrite = send;
+      _xread = recv;
+    }
+  } else if ( prot == sock_base::local ) {
+    _fd = socket( PF_UNIX, type, 0 );
+    if ( _fd == -1 ) {
+      _state |= sock_base::sockfailbit;
+#ifdef WIN32
+      _errno = WSAGetLastError();
+#else
+      _errno = ::errno;
+#endif
+      return 0;
+    }
+  } else { // other protocols not implemented yet
+    return 0;
+  }
+  if ( _base == 0 )
+    _M_allocate_block( 0x2000 ); // max 1460 (dec) --- single segment
+  if ( _base == 0 ) {
+#ifdef WIN32
+    ::closesocket( _fd );
+#else
+    ::close( _fd );
+#endif
+    _fd = -1;
+
+    return 0;
+  }
+
+  setp( _base, _base + ((_ebuf - _base)>>1) );
+  setg( epptr(), epptr(), epptr() );
+
+  __stl_assert( pbase() != 0 );
+  __stl_assert( pptr() != 0 );
+  __stl_assert( epptr() != 0 );
+  __stl_assert( eback() != 0 );
+  __stl_assert( gptr() != 0 );
+  __stl_assert( egptr() != 0 );
+  __stl_assert( _base != 0 );
+  __stl_assert( _ebuf != 0 );
+
+  _state = ios_base::goodbit;
+  _errno = 0; // if any
+  _open = true;
+
+//	in_port_t ppp = 0x5000;
+//	cerr << hex << _address.inet.sin_port << " " << ppp << endl;
+//	cerr << hex << _address.inet.sin_addr.s_addr << endl;
+
+  return this;
+}
 
 template<class charT, class traits>
 basic_sockbuf<charT, traits> *
@@ -15,7 +122,7 @@ basic_sockbuf<charT, traits>::open( SOCKET s, const sockaddr& addr,
   _fd = s;
   memcpy( (void *)&_address.any, (const void *)&addr, sizeof(sockaddr) );
   _mode = ios_base::in | ios_base::out;
-  _state = sock_base::goodbit;
+  _state = ios_base::goodbit;
   _errno = 0;
 #ifdef WIN32
   WSASetLastError( 0 );
@@ -36,7 +143,11 @@ basic_sockbuf<charT, traits>::open( SOCKET s, const sockaddr& addr,
   }
 
   if ( _base == 0 ) {
+#ifdef WIN32
     ::closesocket( _fd );
+#else
+    ::close( _fd );
+#endif
     _fd = -1;
     return 0;
   }
@@ -53,7 +164,7 @@ basic_sockbuf<charT, traits>::open( SOCKET s, const sockaddr& addr,
   __stl_assert( _base != 0 );
   __stl_assert( _ebuf != 0 );
 
-  _state = sock_base::goodbit;
+  _state = ios_base::goodbit;
   _errno = 0; // if any
   _open = true;
 //	in_port_t ppp = 0x5000;
@@ -66,7 +177,6 @@ basic_sockbuf<charT, traits>::open( SOCKET s, const sockaddr& addr,
 //	getpeername( fd(), (sockaddr *)&xxx, &len );
 //	cerr << hex << xxx.sin_port << endl;
 //	cerr << hex << xxx.sin_addr.s_addr << endl;
-	
 
   return this;
 }
@@ -82,11 +192,11 @@ basic_sockbuf<charT, traits>::underflow()
     return traits::to_int_type(*gptr());
 
 #ifdef WIN32
-  fd_set pfd[FD_SETSIZE];
+  fd_set pfd;
   FD_ZERO( &pfd );
   FD_SET( fd(), &pfd );
 
-  if ( select( FD_SETSIZE, &pfd[0], 0, 0, 0 ) <= 0 ) {
+  if ( select( fd() + 1, &pfd, 0, 0, 0 ) <= 0 ) {
     return traits::eof();
   }
 #else
@@ -185,6 +295,44 @@ xsputn( const char_type *s, streamsize n )
     setp( _base, _base + ((_ebuf - _base) >> 1) );
   }
   return n;
+}
+
+template<class charT, class traits>
+int basic_sockbuf<charT, traits>::__rdsync()
+{
+#ifdef WIN32
+  unsigned long nlen = 0;
+  int nmsg = ioctlsocket( fd(), FIONREAD, &nlen );
+#else
+  long nlen = 0;
+  int nmsg = ioctl( fd(), I_NREAD, &nlen );
+#endif
+  if ( nmsg > 0 && nlen > 0 ) {
+    __stl_assert( _base != 0 );
+    __stl_assert( _ebuf != 0 );
+    __stl_assert( gptr() != 0 );
+    __stl_assert( egptr() != 0 );
+
+    cerr << "ioctl: " << dec << nmsg << ", " << nlen << endl;
+    bool shift_req = gptr() == eback() ? false : (_ebuf - gptr()) > nlen ? false : true;
+    if ( shift_req ) {
+      __stl_assert( gptr() > eback() );
+      __stl_assert( gptr() <= egptr() );
+      traits::move( eback(), gptr(), egptr() - gptr() );
+      setg( eback(), eback(), eback() + (egptr() - gptr()) );
+    }
+    if ( gptr() == _ebuf ) { // I should read something, if other side write
+      return -1;             // otherwise I can't write without pipe broken
+    }
+    long offset = (this->*_xread)( egptr(), sizeof(char_type) * (_ebuf - egptr()) );
+    cerr << "I read here " << offset << endl;
+    if ( offset < 0 ) // allow message of zero length
+      return -1;
+    offset /= sizeof(charT);
+    setg( eback(), gptr(), egptr() + offset );
+  }
+
+  return 0;
 }
 
 }
