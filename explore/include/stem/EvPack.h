6b42fcae52ea437f0bf79e5eb0f2338d10903126
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-03-19 18:45:48 +0000

    Initial revision
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@197 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/stem/EvPack.h
@@ -0,0 +1,213 @@
+// -*- C++ -*- Time-stamp: <99/03/19 16:07:17 ptr>
+#ifndef __EvPack_h
+#define __EvPack_h
+
+#ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
+
+#ifdef WIN32
+#  include <winsock2.h>
+#  if defined(BIGENDIAN) && (BIGENDIAN > 0)
+#    define _BIG_ENDIAN
+#  else
+#    define _LITTLE_ENDIAN
+#  endif
+#else
+#  include <sys/isa_defs.h>
+#endif
+
+#ifdef _LITTLE_ENDIAN
+#  include <algorithm>
+#endif
+
+#include <istream>
+#include <ostream>
+
+namespace EDS {
+
+template <int S>
+class __byte_swapper
+{
+  public:
+    static void __byte_swap( char *i )
+      { std::swap( *i, *(i+S-1) );  __byte_swapper<S-2>::__byte_swap( i + 1 ); }
+};
+
+template <>
+class __byte_swapper<1>
+{
+  public:
+    static void __byte_swap( char * )
+      { }
+};
+
+template <>
+class __byte_swapper<0>
+{
+  public:
+    static void __byte_swap( char * )
+      { }
+};
+
+template <class T>
+class __net_converter
+{
+#ifdef _LITTLE_ENDIAN
+    union __swapper
+    {
+        char byte[sizeof(T)];
+        T    value;
+    };
+#endif
+  public:
+    static T to_net( const T& x )
+      {
+#ifdef _LITTLE_ENDIAN
+        __swapper _sw;
+        _sw.value = x;
+        __byte_swapper<sizeof(T)>::__byte_swap( _sw.byte );
+/*
+        int i = 0;
+        int j = sizeof(T) - 1;
+        while ( i < j ) {
+          swap( _sw.byte[i++], _sw.byte[j--] );          
+        }
+*/
+        return _sw.value;
+#else
+        return x;
+#endif
+      }
+
+    static T from_net( const T& x )
+      {
+#ifdef _LITTLE_ENDIAN
+        __swapper _sw;
+        _sw.value = x;
+        __byte_swapper<sizeof(T)>::__byte_swap( _sw.byte );
+/*
+        int i = 0;
+        int j = sizeof(T) - 1;
+        while ( i < j ) {
+          swap( _sw.byte[i++], _sw.byte[j--] );          
+        }
+*/
+        return _sw.value;
+#else
+        return x;
+#endif
+      }
+};
+
+template <>
+class __net_converter<char>
+{
+  public:
+    static char to_net( const char& x )
+      { return x; }
+
+    static char from_net( const char& x )
+      { return x; }
+};
+
+template <>
+class __net_converter<unsigned char>
+{
+  public:
+    static unsigned char to_net( const unsigned char& x )
+      { return x; }
+
+    static unsigned char from_net( const unsigned char& x )
+      { return x; }
+};
+
+template <>
+class __net_converter<signed char>
+{
+  public:
+    static signed char to_net( const signed char& x )
+      { return x; }
+
+    static signed char from_net( const signed char& x )
+      { return x; }
+};
+
+template <class T>
+T to_net( const T& x )
+{ return __net_converter<T>::to_net( x ); }
+
+template <class T>
+T from_net( const T& x )
+{ return __net_converter<T>::from_net( x ); }
+
+struct __pack_base
+{
+    virtual void pack( std::ostream& ) const = 0 ;
+    virtual void unpack( std::istream& ) = 0 ;
+    virtual void net_pack( std::ostream& ) const = 0 ;
+    virtual void net_unpack( std::istream& ) = 0 ;
+
+    static void __net_unpack( std::istream& s, std::string& str );
+    static void __net_pack( std::ostream& s, const std::string& str );
+    static void __unpack( std::istream& s, std::string& str );
+    static void __pack( std::ostream& s, const std::string& str );
+    static void __net_unpack( std::istream& s, int& x )
+      {
+        s.read( (char *)&x, sizeof(int) );
+        x = EDS::from_net( x );
+      }
+    static void __net_pack( std::ostream& s, int x )
+      {
+        x = EDS::to_net( x );
+        s.write( (const char *)&x, 4 );
+      }
+    static void __unpack( std::istream& s, int& x )
+      { s.read( (char *)&x, sizeof(int) ); }
+    static void __pack( std::ostream& s, int x )
+      { s.write( (const char *)&x, 4 ); }
+    static void __net_unpack( std::istream& s, unsigned& x )
+      {
+        s.read( (char *)&x, sizeof(unsigned) );
+        x = EDS::from_net( x );
+      }
+    static void __net_pack( std::ostream& s, unsigned x )
+      {
+        x = EDS::to_net( x );
+        s.write( (const char *)&x, sizeof(unsigned) );
+      }
+    static void __unpack( std::istream& s, unsigned& x )
+      { s.read( (char *)&x, sizeof(unsigned) ); }
+    static void __pack( std::ostream& s, unsigned x )
+      { s.write( (const char *)&x, sizeof(unsigned) ); }
+    static void __net_unpack( std::istream& s, long& x )
+      {
+        s.read( (char *)&x, sizeof(long) );
+        x = EDS::from_net( x );
+      }
+    static void __net_pack( std::ostream& s, long x )
+      {
+        x = EDS::to_net( x );
+        s.write( (const char *)&x, sizeof(long) );
+      }
+    static void __unpack( std::istream& s, long& x )
+      { s.read( (char *)&x, sizeof(long) ); }
+    static void __pack( std::ostream& s, long x )
+      { s.write( (const char *)&x, sizeof(long) ); }
+    static void __net_unpack( std::istream& s, unsigned long& x )
+      {
+        s.read( (char *)&x, sizeof(unsigned long) );
+        x = EDS::from_net( x );
+      }
+    static void __net_pack( std::ostream& s, unsigned long x )
+      {
+        x = EDS::to_net( x );
+        s.write( (const char *)&x, sizeof(unsigned long) );
+      }
+    static void __unpack( std::istream& s, unsigned long& x )
+      { s.read( (char *)&x, sizeof(unsigned long) ); }
+    static void __pack( std::ostream& s, unsigned long x )
+      { s.write( (const char *)&x, sizeof(unsigned long) ); }
+};
+
+}
+
+#endif // __EvPack_h
---
+++ explore/lib/stem/NetTransport.cc
@@ -0,0 +1,150 @@
+// -*- C++ -*- Time-stamp: <99/03/19 18:45:46 ptr>
+
+#ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
+
+#include <iomanip>
+#include <NetTransport.h>
+#include <EventHandler.h>
+#include <EvManager.h>
+
+
+namespace EDS {
+
+using namespace std;
+
+__DLLEXPORT
+void dump( ostream& o, const EDS::Event& e )
+{
+  o << setiosflags(ios_base::showbase) << hex
+    << "Code: " << e.code() << " Destination: " << e.dest() << " Source: " << e.src()
+    << "\nSeq: " << e.seq()
+    << "\nData:\n";
+
+  string mark_line( "-----------| 4 --------| 8 --------| b --------|10 --------|14 --------|\n" );
+  const int ln = 71;
+  char buf[ln + 1];
+
+  unsigned char c;
+  string::const_iterator b = e.value().begin();
+
+  int i = 0;
+  int n = e.value().length();
+  o << mark_line;
+  o << resetiosflags(ios_base::showbase);
+  while ( n-- ) {
+    if ( i == ln ) {
+      o << endl;
+      buf[ln] = 0;
+      o << buf << endl;
+      i = 0;
+    } else if ( i != 0 ) {
+      o << " ";
+      buf[i++] = ' ';
+    }
+    c =  static_cast<unsigned char>(*(b++));
+    o << hex << setw(2) << setfill( '0' ) << unsigned(c);
+    
+    buf[i++] = ' ';
+    buf[i++] = c > 0x1f && c < 0xff ? static_cast<char>(c) : ' ';
+  }
+  if ( i != 0 ) {
+    o << endl;
+    buf[i] = 0;
+    o << buf << endl;   
+  }
+}
+
+__DLLEXPORT
+void NetTransport::connect( sockstream& s, const string& hostname, string& info )
+{
+  cerr << "Connected: " << hostname << endl;
+
+  heap_type::iterator r;
+  Event ev;
+
+  try {
+    net = &s;
+    while ( pop(ev) ) {
+      dump( std::cerr, ev );
+      __stl_assert( EventHandler::_mgr != 0 );
+      // if _mgr == 0, best choice is close connection...
+      r = rar.find( ev._src );
+      if ( r == rar.end() ) {        
+        r = rar.insert( 
+          heap_type::value_type( ev._src,
+          EventHandler::_mgr->SubscribeRemote( this, ev._src, hostname ) ) ).first;
+        cerr << "Create remote object: " << hex << (*r).second
+             << " [" << ev._src << "]\n";
+      }
+      ev._src = (*r).second; // substitute my local id
+
+      EventHandler::_mgr->Dispatch( ev );
+    }
+  }
+  catch ( ios_base::failure& e ) {
+    cerr << "Post mortem: " << e.what() << endl;
+  }
+  catch ( std::runtime_error& e ) {
+    cerr << e.what() << endl;
+  }
+  catch ( std::exception& e ) {
+    cerr << e.what() << endl;
+  }
+  catch ( ... ) {
+    cerr << "What?!" << endl;
+  }
+  cerr << "Disconnected: " << hostname << endl;
+}
+
+bool NetTransport::pop( Event& __rs )
+{
+  unsigned buf[5];
+
+  net->read( (char *)&buf, sizeof(int) * 5 );
+  __rs.code( from_net( buf[0] ) );
+  __rs.dest( from_net( buf[1] ) );
+  __rs.src( from_net( buf[2] ) );
+  __rs.seq( from_net( buf[3] ) );
+
+  unsigned sz = from_net( buf[4] );
+  string& str = __rs.value();
+
+  if ( !net->good() ) {
+    return false;
+  }
+  str.clear();
+  str.reserve( sz );
+  while ( sz-- > 0 ) {
+    str += (char)net->get();
+  }
+
+  return true;
+}
+
+__DLLEXPORT
+bool NetTransport::push( const Event& __rs, const Event::key_type& rmkey,
+                         const Event::key_type& srckey )
+{
+  dump( std::cerr, __rs );
+  std::cerr << "Src key " << hex << srckey << ", remote key " << rmkey
+            << "\n";
+
+  unsigned buf[5];
+
+  buf[0] = to_net( __rs.code() );
+  buf[1] = to_net( /* __rs.dest() */ rmkey );
+  buf[2] = to_net( /* __rs.src() */ srckey );
+  buf[3] = to_net( __rs.seq() != 0 ? __rs.seq() | Event::respbit : ++_count );
+  buf[4] = to_net( __rs.value().size() );
+
+  net->write( (const char *)buf, sizeof(unsigned) * 5 );
+
+  copy( __rs.value().begin(), __rs.value().end(),
+        ostream_iterator<char,char,char_traits<char> >(*net) );
+
+  net->flush();
+
+  return net->good();
+}
+
+} // namespace EDS
