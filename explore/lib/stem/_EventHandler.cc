98dcc440921001912f4724cafb32f236807b6315
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-03-19 18:47:55 +0000

    Reincarnation of EDS. EDS 3 Now! Support net and local events traffic; Total rewritten addressing scheme.
    git-svn-id: svn://localhost/complement/trunk@198 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/_EventHandler.cc
+++ explore/lib/stem/_EventHandler.cc
@@ -1,10 +1,9 @@
-// -*- C++ -*- Time-stamp: <97/09/26 15:39:50 ptr>
+// -*- C++ -*- Time-stamp: <99/03/19 17:13:59 ptr>
 #ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
 
-#include <CLASS/checks.h>
-#include <EDS/EventHandler.h>
+#include <EventHandler.h>
+#include <EvManager.h>
 
-// #include <iostream.h>
 #if defined(__TRACE) || defined(__WARN)
 
 // DIAG_DEFINE_GROUP(EventHandler,0,4);
@@ -12,33 +11,56 @@
 
 #endif // __TRACE || __WARN
 
-EDSEventHandler::evtable_type EDSEventHandler::theEventsTable(
-  "EDSEventHandler", EDSEventHandler::theDeclEventsTable );
+namespace EDS {
 
-__DeclareAnyPMF<EDSEventHandler> EDSEventHandler::theDeclEventsTable[] = {
+EventHandler::evtable_type EventHandler::theEventsTable( EventHandler::theDeclEventsTable );
+
+__DeclareAnyPMF<EventHandler> EventHandler::theDeclEventsTable[] = {
   { 0, 0, { 0, 0, "End of table" }}
 };
 
-DEFINE_NAME_IT( EDSEventHandler );
+char *Init_buf[32];
+EvManager *EventHandler::_mgr = 0;
 
-void EDSEventHandler::PushState( state_type state )
+int EventHandler::Init::_count = 0;
+
+EventHandler::Init::Init()
+{
+  if ( _count++ == 0 ) {
+    _mgr = new EvManager();
+  }
+}
+
+EventHandler::Init::~Init()
+{
+  if ( --_count == 0 ) {
+    delete _mgr;
+  }
+}
+
+void EventHandler::Send( const Event& e )
+{
+  _mgr->Send( e, _id );
+}
+
+void EventHandler::PushState( state_type state )
 {
   RemoveState( state );
   theHistory.push_front( state );
 }
 
-state_type EDSEventHandler::State() const
+state_type EventHandler::State() const
 {
   return theHistory.front();
 }
 
-void EDSEventHandler::PushTState( state_type state )
+void EventHandler::PushTState( state_type state )
 {
   theHistory.push_front( ST_TERMINAL );
   theHistory.push_front( state );
 }
 
-void EDSEventHandler::PopState()
+void EventHandler::PopState()
 {
   theHistory.pop_front();
   while ( theHistory.front() == ST_TERMINAL ) {
@@ -46,7 +68,7 @@ void EDSEventHandler::PopState()
   }
 }
 
-void EDSEventHandler::PopState( state_type state )
+void EventHandler::PopState( state_type state )
 {
   h_iterator hst_i = __find( state );
   if ( hst_i != theHistory.end() && *hst_i != ST_TERMINAL ) {
@@ -54,7 +76,7 @@ void EDSEventHandler::PopState( state_type state )
   }
 }
 
-void EDSEventHandler::RemoveState( state_type state )
+void EventHandler::RemoveState( state_type state )
 {
   h_iterator hst_i = __find( state );
   if ( hst_i != theHistory.end() && *hst_i != ST_TERMINAL ) {
@@ -62,7 +84,7 @@ void EDSEventHandler::RemoveState( state_type state )
   }
 }
 
-bool EDSEventHandler::isState( state_type state ) const
+bool EventHandler::isState( state_type state ) const
 {
   const HistoryContainer& hst = theHistory;
   const_h_iterator hst_i = __find( state );
@@ -72,7 +94,7 @@ bool EDSEventHandler::isState( state_type state ) const
   return false;
 }
 
-h_iterator EDSEventHandler::__find( state_type state )
+h_iterator EventHandler::__find( state_type state )
 {
   if ( theHistory.empty() ) {
     return theHistory.end();
@@ -86,12 +108,12 @@ h_iterator EDSEventHandler::__find( state_type state )
   return hst_i;
 }
 
-const_h_iterator EDSEventHandler::__find( state_type state ) const
+const_h_iterator EventHandler::__find( state_type state ) const
 {
   if ( theHistory.empty() ) {
     return theHistory.end();
   }
-  h_iterator hst_i = theHistory.begin();
+  const_h_iterator hst_i = theHistory.begin();
 
   while ( hst_i != theHistory.end() && *hst_i != ST_TERMINAL
 	  && *hst_i != state ) {
@@ -106,36 +128,51 @@ const_h_iterator EDSEventHandler::__find( state_type state ) const
 // Of cause, you can found this only if theHistory is a template with static
 // member (stl/list, for example).
 
-EDSEventHandler::EDSEventHandler() :
-    theHistory( *(new HistoryContainer()) )
+EventHandler::EventHandler()// :
+//    theHistory( *(new HistoryContainer()) )
 {
+  new( Init_buf ) Init();
   State( ST_NULL );
+  _id = _mgr->Subscribe( this, "" );
 }
 
-EDSEventHandler::~EDSEventHandler()
+EventHandler::EventHandler( Event::key_type id )// :
+//    theHistory( *(new HistoryContainer()) )
 {
-  delete &theHistory;
+  new( Init_buf ) Init();
+  State( ST_NULL );
+  _id = _mgr->SubscribeID( id, this, "" );
+  __stl_assert( _id != -1 ); // already registered, or id has Event::extbit
+//  if ( _id == -1 ) {
+//    _mgr->Subscribe( this, "" );
+//  }
 }
 
-bool EDSEventHandler::Dispatch( const EDSEvent& event )
+EventHandler::~EventHandler()
+{
+//  delete &theHistory;
+   _mgr->Unsubscribe( _id );
+  ((Init *)Init_buf)->~Init();
+}
+
+bool EventHandler::Dispatch( const Event& event )
 {
   return theEventsTable.Dispatch( this, theHistory.begin(),theHistory.end(),
 				  event );
 }
 
-bool EDSEventHandler::DispatchStub( const EDSEvent& event )
+bool EventHandler::DispatchStub( const Event& event )
 {
   return theEventsTable.DispatchStub( this, theHistory.begin(),
 				      theHistory.end(), event );
 }
-void EDSEventHandler::DispatchTrace( const EDSEvent& __event__,
-				     ostrstream& out )
+void EventHandler::DispatchTrace( const Event& __event__, ostream& out )
 {
   theEventsTable.DispatchTrace( theHistory.begin(),theHistory.end(),
 				__event__, out );
 }
 
-void EDSEventHandler::TraceStack( ostrstream& out ) const
+void EventHandler::TraceStack( ostream& out ) const
 {
   const HistoryContainer& hst = theHistory;
   HistoryContainer::const_iterator hst_i = hst.begin();
@@ -145,7 +182,9 @@ void EDSEventHandler::TraceStack( ostrstream& out ) const
   out << endl;
 }
 
-void EDSEventHandler::Trace( ostrstream& out ) const
+void EventHandler::Trace( ostream& out ) const
 {
-  EDSEventHandler::theEventsTable.Out( out );
+  EventHandler::theEventsTable.Out( out );
 }
+
+} // namespace EDS
