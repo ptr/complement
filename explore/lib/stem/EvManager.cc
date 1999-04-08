fa4ff5e5ff44a3dddc1284a62c7c4c9c2b152e27
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-04-08 14:20:53 +0000

    After connection lost EvManager send (to controlling address, if any) EV_DISCONNECT with session id.
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@253 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/EvManager.cc
+++ explore/lib/stem/EvManager.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <99/03/26 20:37:52 ptr>
+// -*- C++ -*- Time-stamp: <99/04/06 19:15:34 ptr>
 #ident "%Z% $Date$ $Revision$ $RCSfile$ %Q%"
 
 #include <EvManager.h>
@@ -8,9 +8,13 @@ namespace EDS {
 
 std::string EvManager::inv_key_str( "invalid key" );
 
+// Remove references to remote objects, that was announced via 'channel'
+// (related, may be, with socket connection)
+// from [remote name -> local name] mapping table, and mark related session as
+// 'disconnected'.
 void EvManager::Remove( NetTransport *channel )
 {
-  MT_REENTRANT( _lock_heap, _1 );
+  MT_LOCK( _lock_heap );
   heap_type::iterator i = heap.begin();
 
   while ( i != heap.end() ) {
@@ -20,9 +24,12 @@ void EvManager::Remove( NetTransport *channel )
       ++i;
     }
   }
+  MT_UNLOCK( _lock_heap );
   disconnect( channel->sid() );
 }
 
+// return session id of object with address 'id' if this is external
+// object; otherwise return -1;
 EvSessionManager::key_type EvManager::sid( const key_type& id ) const
 {
   MT_REENTRANT( _lock_heap, _1 );
@@ -33,6 +40,9 @@ EvSessionManager::key_type EvManager::sid( const key_type& id ) const
   return (*i).second.remote->channel->sid();
 }
 
+// Local access to object from remote call: only local object may be here;
+// This function used only from NetTransport after mapping remote object
+// address to its local address. (Incoming remote events only).
 void EvManager::Dispatch( const Event& e )
 {
   heap_type::iterator i = heap.find( e.dest() );
@@ -41,6 +51,9 @@ void EvManager::Dispatch( const Event& e )
   }
 }
 
+// Resolve Address -> Object Reference, call Object's dispatcher in case
+// of local object, or call appropriate channel delivery function for
+// remote object. (Outcoming local and remote events).
 void EvManager::Send( const Event& e, const EvManager::key_type& src_id )
 {
   // Will be useful to block on erase/insert operations...
@@ -62,9 +75,6 @@ void EvManager::Send( const Event& e, const EvManager::key_type& src_id )
 
 EvManager::key_type EvManager::create_unique()
 {
-  const key_type _low  = 0x100;
-  const key_type _high = /* 65535 */ 0x3fffffff;
-
   do {
     if ( ++_id > _high ) {
       _id = (_id - _low) % (_high - _low) + _low;
@@ -84,7 +94,18 @@ EvSessionManager::key_type EvManager::establish_session()
 void EvManager::disconnect( const EvSessionManager::key_type& _sid )
 {
   if ( smgr.is_avail( _sid ) ) {
-    smgr[_sid].disconnect();
+    SessionInfo& info = smgr[_sid];
+    info.disconnect();
+//    cerr << "EvManager::disconnect: " << _sid << endl;
+    if ( info._control != Event::badaddr ) {
+      Event_base<Event::key_type> ev_disconnect( EV_DISCONNECT, _sid );
+      ev_disconnect.dest( info._control );
+//      cerr << "EvManager::disconnect, info._control: " << info._control << endl;
+      Send( EDS::Event_convert<Event::key_type>()(ev_disconnect), Event::mgraddr );
+//      cerr << "===== Pass" << endl;
+    } else {
+      smgr.erase( _sid );
+    }
   }
 }
 
