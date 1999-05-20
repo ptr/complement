6228f082f8ae78b0d6b3f5f88395958a42df9374
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-05-20 13:33:24 +0000

    Initial revision
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@284 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/lib/mt/thr_mgr.cc
@@ -0,0 +1,59 @@
+// -*- C++ -*- Time-stamp: <99/05/20 13:31:06 ptr>
+#ident "$SunId$ %Q%"
+
+#include <thr_mgr.h>
+#include <algorithm>
+
+namespace __impl {
+
+struct bad_thread :
+    public unary_function<Thread *,bool>
+{
+    bool operator()(const Thread *__x) const
+      { return !__x->good(); }
+};
+
+__DLLEXPORT ThreadMgr::~ThreadMgr()
+{
+  MT_REENTRANT( _lock, _1 );
+  container_type::iterator i = _M_c.begin();
+
+  while ( i != _M_c.end() ) {
+    if ( (*i)->good() ) {
+      (*i)->kill( SIGTERM );
+    }
+    delete *i;
+    _M_c.erase( i++ );
+  }
+}
+
+__DLLEXPORT
+void ThreadMgr::launch( Thread::entrance_type entrance,
+                        const void *p = 0, size_t psz = 0 )
+{
+  MT_REENTRANT( _lock, _1 );
+  container_type::iterator i = find_if( _M_c.begin(), _M_c.end(), bad_thread() );
+  if ( i = _M_c.end() ) {
+    _M_c.push_back( new Thread() );
+    i = --_M_c.end();
+  }
+  (*i)->launch( entrance, p, psz );
+}
+
+__DLLEXPORT
+void ThreadMgr::garbage_collector()
+{
+  MT_REENTRANT( _lock, _1 );
+  container_type::iterator i = _M_c.begin();
+
+  while ( i != _M_c.end() ) {
+    if ( !(*i)->good() ) {
+      delete *i;
+      _M_c.erase( i++ );
+    } else {
+      ++i;
+    }
+  }
+}
+
+} // namespace __impl
