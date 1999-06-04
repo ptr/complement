75b606e1d91b977190a83b95672099e5c752d395
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-06-04 14:05:39 +0000

    Initial revision
    git-svn-id: svn://localhost/complement/trunk@311 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/stem/EDSEv.h
@@ -0,0 +1,19 @@
+// -*- C++ -*- Time-stamp: <99/06/04 13:07:42 ptr>
+
+#ifndef __EDSEv_h
+#define __EDSEv_h
+
+#ident "$SunId$ %Q%"
+
+namespace EDS {
+
+#define EV_EDS_CONNECT       0x06
+#define EV_EDS_DISCONNECT    0x07
+#define EV_EDS_RQ_SESSION    0x10
+#define EV_EDS_RRQ_SESSION   0x11
+#define EV_EDS_RS_SESSION    0x12
+#define EV_EDS_CL_SESSION    0x13
+
+} // namespace EDS
+
+#endif // __EDSEv_h
