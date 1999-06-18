7920e46864d1b7f9cea302a139e9da3c163a177c
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-06-18 17:31:25 +0000

    EV_EDS_ANNOUNCE added
    git-svn-id: svn://localhost/complement/trunk@325 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/include/stem/EDSEv.h
+++ explore/include/stem/EDSEv.h
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <99/06/04 13:07:42 ptr>
+// -*- C++ -*- Time-stamp: <99/06/09 11:12:07 ptr>
 
 #ifndef __EDSEv_h
 #define __EDSEv_h
@@ -9,6 +9,7 @@ namespace EDS {
 
 #define EV_EDS_CONNECT       0x06
 #define EV_EDS_DISCONNECT    0x07
+#define EV_EDS_ANNOUNCE      0x08
 #define EV_EDS_RQ_SESSION    0x10
 #define EV_EDS_RRQ_SESSION   0x11
 #define EV_EDS_RS_SESSION    0x12
