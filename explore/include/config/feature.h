476c97fd3517ca03f7635b1a7f6cfc0cc92e2474
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
1999-10-28 16:54:34 +0000

    in-class operators
    git-svn-id: svn://localhost/complement/trunk@405 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/include/config/feature.h
+++ explore/include/config/feature.h
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <99/10/04 10:50:21 ptr>
+// -*- C++ -*- Time-stamp: <99/10/18 18:32:39 ptr>
 
 /*
  *
@@ -71,7 +71,7 @@
 #  ifndef __LINK_TIME_INSTANTIATION
 #    define __LINK_TIME_INSTANTIATION
 #  endif
-// #  define __PG_INCLASS_OPERATOR
+#  define __PG_INCLASS_OPERATOR
 #endif // __SUNPRO_CC
 
 #endif // __config_feature_h
