ce76108d36d0cc011fa86bcb29354076967308ab
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2000-01-05 12:08:12 +0000

    Two DB supported: Postgres and MySQL
    git-svn-id: svn://localhost/complement/trunk@416 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/DB/SQL.cc
+++ explore/lib/DB/SQL.cc
@@ -1,12 +1,17 @@
-// -*- C++ -*- Time-stamp: <99/11/12 17:44:29 ptr>
+// -*- C++ -*- Time-stamp: <99/12/06 11:54:43 ptr>
 
 #ident "$SunId$ %Q%"
 
 #include <config/feature.h>
 #include "DB/SQL.h"
-#include <cstdio>
+
+#if defined( __DB_POSTGRES )
+#  include <cstdio>
 using std::FILE;
-#include <libpq-fe.h>
+#  include <libpq-fe.h>
+#elif defined( __DB_MYSQL )
+#  include <mysql.h>
+#endif
 
 #include <iostream>
 #include <iomanip>
@@ -25,7 +30,11 @@ void Table::done()
   bool is_first = true;
   switch ( _op ) {
     case _INSERT:
+#if defined( __DB_POSTGRES )
       s << "INSERT INTO \"" << _name << "\" (";
+#elif defined( __DB_MYSQL )
+      s << "INSERT INTO " << _name << " (";
+#endif
       for ( int i = 0; i < header.size(); ++i ) {
         if ( header[i].second == _NILL ) {
           continue;
@@ -35,7 +44,11 @@ void Table::done()
         } else {
           is_first = false;
         }
+#if defined( __DB_POSTGRES )
         s << "\"" << header[i].first << "\"";
+#elif defined( __DB_MYSQL )
+        s << header[i].first;
+#endif
       }
       is_first = true;
       s << ") VALUES (";
@@ -59,23 +72,47 @@ void Table::done()
   }
 }
 
-DataBase::DataBase( const char *name,
+DataBase::DataBase( const char *name, const char *usr, const char *passwd,
                     const char *host, const char *port,
                     const char *opt, const char *tty ) :
     _flags( 0 )
-{  
-  _conn = PQsetdbLogin( host, port, opt, tty, name, 0, 0 );
+{
+#if defined( __DB_POSTGRES )
+  string db_name_quoted = "\"";
+  db_name_quoted += name;
+  db_name_quoted += "\"";
+  _conn = PQsetdbLogin( host, port, opt, tty, /* name */ db_name_quoted.c_str(), usr, passwd );
   if ( PQstatus( _conn ) == CONNECTION_BAD ) {
     _flags = badbit | failbit;
     PQfinish( _conn );
     _conn = 0;
   }
+#elif defined( __DB_MYSQL )
+  _conn = new DBconn;
+  mysql_init( _conn );
+  int _port = 0;
+  if ( port != 0 ) {
+    istringstream is( port );
+    is >> _port;
+  }
+  if ( !mysql_real_connect( _conn, host, usr, passwd, name, _port, 0, 0 ) ) {
+    _flags = badbit | failbit;
+    mysql_close( _conn );
+    delete _conn;
+    _conn = 0;
+  }
+#endif
 }
 
 DataBase::~DataBase()
 {
   if ( _flags == goodbit ) {
+#if defined( __DB_POSTGRES )
     PQfinish( _conn );
+#elif defined( __DB_MYSQL )
+    mysql_close( _conn );
+    delete _conn;
+#endif
     _conn = 0;
     _flags = badbit;
   }
@@ -87,12 +124,18 @@ void DataBase::exec( const string& s )
     return;
   }
 
+#if defined( __DB_POSTGRES )
   PGresult *_result = PQexec( _conn, s.c_str() );
   if ( _result == 0 || PQresultStatus( _result ) != PGRES_COMMAND_OK ) {
     _flags = failbit;
   }
 
   PQclear( _result );
+#elif defined( __DB_MYSQL )
+  if ( mysql_query( _conn, s.c_str() ) ) {
+    _flags = failbit;
+  }
+#endif
 }
 
 void DataBase::exec()
@@ -101,12 +144,18 @@ void DataBase::exec()
     return;
   }
 
+#if defined( __DB_POSTGRES )
   PGresult *_result = PQexec( _conn, sentence.c_str() );
   if ( _result == 0 || PQresultStatus( _result ) != PGRES_COMMAND_OK ) {
     _flags = failbit;
   }
 
   PQclear( _result );
+#elif defined( __DB_MYSQL )
+  if ( mysql_query( _conn, sentence.c_str() ) ) {
+    _flags = failbit;
+  }
+#endif
 }
 
 Table& DataBase::define_table( const char *nm )
@@ -128,12 +177,20 @@ Table& DataBase::table( const char *nm )
 
 void DataBase::begin_transaction()
 {
+#if defined( __DB_POSTGRES )
   sentence = "BEGIN";
+#elif defined( __DB_MYSQL )
+  sentence.clear(); // LOCK?
+#endif
 }
 
 void DataBase::end_transaction()
 {
+#if defined( __DB_POSTGRES )
   sentence += "; COMMIT";
+#elif defined( __DB_MYSQL )
+
+#endif
   this->exec();
 }
 
@@ -141,16 +198,24 @@ Transaction::Transaction( DataBase& db ) :
     _db( db )
 {
   if ( _db.good() ) {
+#if defined( __DB_POSTGRES )
     _exec_str = "BEGIN";
     _db.exec( _exec_str );
+#elif defined( __DB_MYSQL )
+
+#endif
   }
 }
 
 Transaction::~Transaction()
 {
   if ( _db.good() ) {
+#if defined( __DB_POSTGRES )
     _exec_str = "COMMIT";
     _db.exec( _exec_str );
+#elif defined( __DB_MYSQL )
+
+#endif
   }
 }
 
@@ -158,23 +223,32 @@ Cursor::Cursor( Transaction& t , const char *nm, const char *select ) :
     name( nm ),
     tr( t )
 {
+#if defined( __DB_POSTGRES )
   string tmp = "DECLARE ";
   tmp += name;
   tmp += " CURSOR FOR ";
   tmp += select;
   tr._db.exec( tmp );
   cerr << tmp << endl;
+#elif defined( __DB_MYSQL )
+  name = select;
+#endif
 }
 
 Cursor::~Cursor()
 {
+#if defined( __DB_POSTGRES )
   string tmp = "CLOSE ";
   tmp += name;
   tr._db.exec( tmp );
+#elif defined( __DB_MYSQL )
+
+#endif
 }
 
 void Cursor::fetch_all()
 {
+#if defined( __DB_POSTGRES )
   string tmp = "FETCH ALL IN ";
   tmp += name;
   PGresult *_result = PQexec( tr._db._conn, tmp.c_str() );
@@ -201,6 +275,45 @@ void Cursor::fetch_all()
     }
   }
   PQclear( _result );
+#elif defined( __DB_MYSQL )
+  tr._db.exec( name );
+  cerr << name << endl;
+  MYSQL_RES *_result = mysql_store_result( tr._db._conn );
+
+  if ( _result == 0 ) {
+    if ( mysql_errno( tr._db._conn ) ) {
+      tr._db._flags = DataBase::failbit;
+    } else if ( mysql_field_count( tr._db._conn ) == 0 ) {
+      // not select
+      ;
+    }
+    fields.clear();
+    return;
+  }
+
+  fields.clear();
+  int n = mysql_num_fields( _result );
+  fields.reserve( n );
+
+  for ( int i = 0; i < n; ++i ) {
+    fields.push_back( mysql_fetch_field( _result )->name );
+  }
+  int ntuples = mysql_num_rows( _result );
+
+  data.clear();
+  data.reserve( ntuples );
+
+  MYSQL_ROW row;
+  for ( int j = 0; j < ntuples; ++j ) {
+    data.push_back();
+    data[j].reserve( n );
+    row = mysql_fetch_row( _result );
+    for ( int i = 0; i < n; ++i ) {
+      data[j].push_back( row[i] );
+    }
+  }
+  mysql_free_result( _result );
+#endif
 }
 
 const string& Cursor::value( int n, const string& f )
