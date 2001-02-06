7e4905757ee453e62557980e4921552e8bbfd471
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2001-02-06 10:19:21 +0000

    Added copy constructor and assignment operator. All MT-unsafe operations are protected now. Public accesable operations return copy of object, not reference.
    git-svn-id: svn://localhost/complement/trunk@569 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/_EvSession.cc
+++ explore/lib/stem/_EvSession.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <00/09/11 14:45:23 ptr>
+// -*- C++ -*- Time-stamp: <01/02/02 13:01:51 ptr>
 
 /*
  * Copyright (c) 1995-1999
@@ -28,6 +28,50 @@
 #include <EDS/EvSession.h>
 
 namespace EDS {
+
+SessionInfo::SessionInfo( const SessionInfo& si )
+{
+  _host = si._host;
+  _port = si._port;
+
+  _sz_from = si._sz_from;
+  _un_from = si._un_from;
+  _sz_to = si._sz_to;
+  _un_to = si._un_to;
+  _lun_from = si._lun_from;
+  _lun_to = si._lun_to;
+  _start = si._start;
+  _on_line = si._on_line;
+  _conn = si._conn;
+  _last = si._last;
+  _reconnect_cnt = si._reconnect_cnt;
+  _is_connected = si._is_connected;
+
+  _control = si._control;
+}
+
+SessionInfo& SessionInfo::operator =( const SessionInfo& si )
+{
+  _host = si._host;
+  _port = si._port;
+
+  _sz_from = si._sz_from;
+  _un_from = si._un_from;
+  _sz_to = si._sz_to;
+  _un_to = si._un_to;
+  _lun_from = si._lun_from;
+  _lun_to = si._lun_to;
+  _start = si._start;
+  _on_line = si._on_line;
+  _conn = si._conn;
+  _last = si._last;
+  _reconnect_cnt = si._reconnect_cnt;
+  _is_connected = si._is_connected;
+
+  _control = si._control;
+
+  return *this;
+}
 
 #if !defined(__HP_aCC) || (__HP_aCC > 1)
 template class SessionManager<addr_type>;
