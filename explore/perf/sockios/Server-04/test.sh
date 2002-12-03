2558beceb79077d04bc8ac863cc2547546e7d030
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2002-12-03 08:29:38 +0000

    Fix difference in echo for Linux and Solaris
    git-svn-id: svn://localhost/complement/trunk@727 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/perf/sockios/Server-04/test.sh
+++ explore/perf/sockios/Server-04/test.sh
@@ -8,7 +8,14 @@ d=`dirname $d`/build/lib
 LD_LIBRARY_PATH=$d:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH
 
-echo=echo
+case `uname` in
+  Linux)
+    echo="echo -e"
+    ;;
+  SunOS)
+    echo=echo
+    ;;
+esac
 
 w_lnum () {
   if [ -f $1 ] ; then
@@ -25,7 +32,6 @@ experiment () {
   srv=$!
   # start client
   w_lnum client-$3.log
-  sleep 2
   ${time} -a -o client-$3.log ../Client-04/obj/gcc/shared/client -p=1990 -host=localhost -n=$2 -b=$1
   wait $srv
   sleep 1
@@ -58,9 +64,9 @@ nb=50
 lh=52428800
 nh=1
 
-# repeat 10 experiment $ls $ns s
-# repeat 10 experiment $l0 $n0 0
-# repeat 10 experiment $lp $np p
+repeat 10 experiment $ls $ns s
+repeat 10 experiment $l0 $n0 0
+repeat 10 experiment $lp $np p
 repeat 10 experiment $lb $nb b
 repeat 10 experiment $lh $nh h
 
@@ -98,3 +104,45 @@ ${echo} "$lh \c"
 ${echo} "# Client $lh/$nh"
 ${echo} "$lh \c"
 ../Server-03/stat.awk client-h.log
+
+
+
+${echo} "-----------"
+
+${echo} "# Server " `date` `uname -a`
+${echo} "# $ls/$ns"
+${echo} "$ls \c"
+../Server-03/stat.awk server-s.log
+${echo} "# $l0/$n0"
+${echo} "$l0 \c"
+../Server-03/stat.awk server-0.log
+${echo} "# $lp/$np"
+${echo} "$lp \c"
+../Server-03/stat.awk server-p.log
+${echo} "# $lb/$nb"
+${echo} "$lb \c"
+../Server-03/stat.awk server-b.log
+${echo} "# Server $lh/$nh"
+${echo} "$lh \c"
+../Server-03/stat.awk server-h.log
+
+${echo} "-----------"
+
+${echo} "# Client " `date` `uname -a`
+${echo} "# ls/$ns"
+${echo} "$ls \c"
+../Server-03/stat.awk client-s.log
+${echo} "# $l0/$n0"
+${echo} "$l0 \c"
+../Server-03/stat.awk client-0.log
+${echo} "# $lp/$np"
+${echo} "$lp \c"
+../Server-03/stat.awk client-p.log
+${echo} "# Client $lb/$nb"
+${echo} "$lb \c"
+../Server-03/stat.awk client-b.log
+${echo} "# Client $lh/$nh"
+${echo} "$lh \c"
+../Server-03/stat.awk client-h.log
+
+${echo} "-----------"
