c10c787f90983d4370c0f423c0d8c3c8cb971fe1
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2003-01-19 17:30:22 +0000

    Separate OS from each other and compilers from OS
    git-svn-id: svn://localhost/complement/trunk@750 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/config/_gcc.h
@@ -0,0 +1,34 @@
+// -*- C++ -*- Time-stamp: <03/01/19 13:22:52 ptr>
+
+/*
+ *
+ * Copyright (c) 2003
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 1.0
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ *
+ */
+
+#ifndef __config__gcc_h
+#define __config__gcc_h
+
+#ident "@(#)$Id$"
+
+// #if defined(__GNUC__) && (__GNUC__==3) // gcc 3.1.1 at least
+// #  define __FIT_TYPENAME_TEMPLATE_PARAMETER_RET
+// #endif
+#if defined(__GNUC__) && ( __GNUC__ == 2 ) && (__GNUC_MINOR__ == 95)
+  // at least 2.95.3
+// #define __FIT_STD_AS_GLOBAL
+#endif
+
+#endif // __config__gcc_h
