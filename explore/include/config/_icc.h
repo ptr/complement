fecc20a8bba4f166e7cd594e4888ce320b422aad
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2004-03-09 14:19:29 +0000

    support Intel compiler
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@940 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/include/config/_icc.h
@@ -0,0 +1,26 @@
+// -*- C++ -*- Time-stamp: <03/01/19 13:22:52 ptr>
+
+/*
+ *
+ * Copyright (c) 2004
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 2.0
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ *
+ */
+
+#ifndef __config__icc_h
+#define __config__icc_h
+
+#ident "@(#)$Id$"
+
+#endif // __config__icc_h
