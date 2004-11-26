948f9122473a2eadc01649019e75cbcc89cec58b
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2004-11-26 09:25:11 +0000

    development of locale implementation in STLport
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1041 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/inquiry/STLport/locale_impl/test.cc
@@ -0,0 +1,12 @@
+// -*- C++ -*- Time-stamp: <02/09/25 12:11:17 ptr>
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char * const *argv )
+{
+  // cerr << "Hello, world!" << endl;
+
+  return 0;
+}
