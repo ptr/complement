94f4e028a893875f2930e752d73c86caf7618242
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2005-09-11 08:09:51 +0000

    add license notification
    git-svn-id: svn://localhost/complement/trunk@1111 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/mt/fl.c
+++ explore/lib/mt/fl.c
@@ -1,12 +1,21 @@
-/* -*- C -*- Time-stamp: <04/05/18 09:06:09 ptr> */
+/* -*- C -*- Time-stamp: <05/09/02 00:35:40 ptr> */
 
-#if defined(__unix) || defined(__unix__)
-#  ifdef __HP_aCC
-#pragma VERSIONID "@(#)$Id$"
-#  else
-#ident "@(#)$Id$"
-#  endif
-#endif
+/*
+ * Copyright (c) 2004
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 2.1
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ *
+ */
 
 #include <config/feature.h>
 
