c215d26ce54ac6c369f4d1376f6bde563ed2ab48
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2005-12-12 08:12:10 +0000

    redefinition of functions that will be replaced by reentrant ones
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1130 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/lib/sockios/freebsd/_freebsd_dummy.h
@@ -0,0 +1,45 @@
+#ifndef ___freebsd_dummy_h
+#define ___freebsd_dummy_h
+
+#include <config/feature.h>
+
+#ifdef __FIT_NONREENTRANT
+
+#define skiprr            x_skiprr
+#define ns_initparse      x_ns_initparse
+#define dn_skipname       x_dn_skipname
+#define dn_expand         x_dn_expand
+#define dn_comp           x_dn_comp
+
+#define res_search        x_res_search
+#define res_query         x_res_query
+#define res_querydomain   x_res_querydomain
+#define hostalias         x_hostalias
+#define res_init          x_res_init
+#define res_mkquery       x_res_mkquery
+#define res_opt           x_res_opt
+#define res_send          x_res_send
+#define res_isourserver   x_res_isourserver
+#define res_nameinquery   x_res_nameinquery
+#define res_queriesmatch  x_res_queriesmatch
+#define res_update        x_res_update
+#define res_dnok          x_res_dnok
+#define res_mailok        x_res_mailok
+#define res_ownok         x_res_ownok
+#define res_hnok          x_res_hnok
+
+#define res_send_setqhook x_res_send_setqhook
+#define res_send_setrhook x_res_send_setrhook
+
+#define get_nsaddr        x_get_nsaddr
+
+#define res_close         x_res_close
+
+#define getaddrinfo       x_getaddrinfo
+#define freeaddrinfo      x_freeaddrinfo
+
+#define gai_strerror      x_gai_strerror
+
+#endif /* __FIT_NONREENTRANT */
+
+#endif /* ___freebsd_dummy_h */
