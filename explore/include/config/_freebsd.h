7d82996576a39edf4b5d83a40a27fcf4bfc26534
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2005-12-12 08:14:17 +0000

    FreeBSD 4.11 has non-reentrant res_*, getaddrinfor, ns_* functions; we can use reentrant ones from FreeBSD 5.3 libc instead
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1131 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/include/config/_freebsd.h
+++ explore/include/config/_freebsd.h
@@ -1,8 +1,8 @@
-// -*- C++ -*- Time-stamp: <05/09/02 00:46:35 ptr>
+/*  Time-stamp: <05/12/12 10:46:44 ptr> */
 
 /*
  *
- * Copyright (c) 2003
+ * Copyright (c) 2003, 2005
  * Petr Ovtchenkov
  *
  * Licensed under the Academic Free License Version 2.1
@@ -21,7 +21,10 @@
 #ifndef __config__freebsd_h
 #define __config__freebsd_h
 
-#define __FIT__P_PROBLEM // Hide __P from sys/cdefs.h. Workaround for glibc.
+/* Endiannes */
+#include <sys/types.h>
+
+#define __FIT__P_PROBLEM /* Hide __P from sys/cdefs.h. Workaround for glibc. */
 
 #if defined(_REENTRANT) && !defined(_PTHREADS)
 #  define _PTHREADS
@@ -31,27 +34,34 @@
 #  ifndef __USE_UNIX98
 #    define __USE_UNIX98
 #  endif
-#  define __FIT_XSI_THR  // Unix 98 or X/Open System Interfaces Extention
-// The IEEE Std. 1003.1j-2000 introduces functions to implement spinlocks.
-#  define __FIT_PTHREAD_SPINLOCK
+#  define __FIT_XSI_THR  /* Unix 98 or X/Open System Interfaces Extention */
+/* The IEEE Std. 1003.1j-2000 introduces functions to implement spinlocks. */
+#  if __FreeBSD_version >= 503001
+#    define __FIT_PTHREAD_SPINLOCK
+#  endif
 #  define __FIT_RWLOCK
 #endif
 
-// Endiannes
-#include <sys/types.h>
-// #if !defined(BYTE_ORDER) || !defined(LITTLE_ENDIAN) || !defined(BIG_ENDIAN)
-// #  error "One of __BYTE_ORDER, __LITTLE_ENDIAN and __BIG_ENDIAN undefined; Fix me!"
-// #endif
+/*
+#if !defined(BYTE_ORDER) || !defined(LITTLE_ENDIAN) || !defined(BIG_ENDIAN)
+#  error "One of __BYTE_ORDER, __LITTLE_ENDIAN and __BIG_ENDIAN undefined; Fix me!"
+#endif
 
-// #if (BYTE_ORDER == LITTLE_ENDIAN)
-// #  define _LITTLE_ENDIAN
-// #elif (BYTE_ORDER == BIG_ENDIAN)
-// #  define _BIG_ENDIAN
-// #else
-// #  error "BYTE_ORDER neither BIG_ENDIAN nor LITTLE_ENDIAN; Fix me!"
-// #endif
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+#  define _LITTLE_ENDIAN
+#elif (BYTE_ORDER == BIG_ENDIAN)
+#  define _BIG_ENDIAN
+#else
+#  error "BYTE_ORDER neither BIG_ENDIAN nor LITTLE_ENDIAN; Fix me!"
+#endif
+*/
 
-// No gethostbyname_r and gethostbyaddr_r functions
+/* No gethostbyname_r and gethostbyaddr_r functions */
 #define __FIT_GETHOSTBYADDR
 
-#endif // __config__freebsd_h
+#if (__FreeBSD_version < 503001)
+/* use workaround for non-reentrant res_*, ns_*, getaddrinfo functions */
+#  define __FIT_NONREENTRANT
+#endif
+
+#endif /* __config__freebsd_h */
