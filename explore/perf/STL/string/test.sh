44928423cfe026d9aa0e609efeca4543f94f1ff9
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2005-12-30 11:51:05 +0000

    he-he, var scope in shell isn't same as in C
    git-svn-id: svn://localhost/complement/trunk@1160 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/perf/STL/string/test.sh
+++ explore/perf/STL/string/test.sh
@@ -2,8 +2,6 @@
 #
 # Time-stamp: <04/07/15 01:16:23 ptr>
 #
-# $Id$
-#
 
 BASEDIR=${PWD}/../../..
 timeprg=${BASEDIR}/app/utils/time/obj/gcc/shared/time
@@ -25,54 +23,54 @@ function runtest ()
 rm -f libstd++.dat
 touch libstd++.dat
 
-i=1
+j=1
 for d in add find ops params params-ref params-short ; do
   runtest ${d}/libstd++
-  echo $i `../stat.awk s.log` >> libstd++.dat
-  let i=i+1
+  echo $j `../stat.awk s.log` >> libstd++.dat
+  let j=j+1
 done
 # dummy:
-echo $i `../stat.awk s.log` >> libstd++.dat
+echo $j `../stat.awk s.log` >> libstd++.dat
 
 rm -f STLport.dat
 touch STLport.dat
 
-i=1
+j=1
 for d in add find ops params params-ref params-short ; do
   runtest ${d}/STLport-default
-  echo $i `../stat.awk s.log` >> STLport.dat
-  let i=i+1
+  echo $j `../stat.awk s.log` >> STLport.dat
+  let j=j+1
 done
 runtest params-short/STLport-no-short-str
-echo $i `../stat.awk s.log` >> STLport.dat
+echo $j `../stat.awk s.log` >> STLport.dat
 
 rm -f STLport-malloc.dat
 touch STLport-malloc.dat
 
-i=1
+j=1
 for d in add find ops params params-ref ; do
   runtest ${d}/STLport-malloc
-  echo $i `../stat.awk s.log` >> STLport-malloc.dat
-  let i=i+1
+  echo $j `../stat.awk s.log` >> STLport-malloc.dat
+  let j=j+1
 done
 
 rm -f STLport-newalloc.dat
 touch STLport-newalloc.dat
 
-i=1
+j=1
 for d in add find ops params params-ref ; do
   runtest ${d}/STLport-newalloc
-  echo $i `../stat.awk s.log` >> STLport-newalloc.dat
-  let i=i+1
+  echo $j `../stat.awk s.log` >> STLport-newalloc.dat
+  let j=j+1
 done
 
 rm -f STLport-ropes.dat
 touch STLport-ropes.dat
 
-i=1
+j=1
 for d in add find ops params params-ref ; do
   runtest ${d}-ropes/STLport-default
-  echo $i `../stat.awk s.log` >> STLport-ropes.dat
-  let i=i+1
+  echo $j `../stat.awk s.log` >> STLport-ropes.dat
+  let j=j+1
 done
 
