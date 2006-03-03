79a9d457e76d3f0418a421ca338db7355625bc83
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2006-03-03 12:50:47 +0000

    test for complex aggregate (strings) problem in STLport
    git-svn-id: svn://localhost/complement/trunk@1190 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/inquiry/STLport/string-static-array/test.cc
@@ -0,0 +1,54 @@
+#include <string>
+#include <iostream>
+// #include <iomanip>
+
+using namespace std;
+
+struct A
+{
+  A() { cerr << "A()\n"; }
+  A( int j ) : i(j) { cerr << "A( int )\n"; }
+  A( const A& a ) : i(a.i) { cerr << "A( const A& )\n"; }
+  A& operator =( const A& a ) { i = a.i; cerr << "operator =( const A& )\n"; return *this; }
+  A& operator =( int j ) { i = j; cerr << "operator =( int )\n"; return *this; }
+
+  ~A() { cerr << "~A()\n"; }
+
+  int i;
+};
+
+struct B
+{
+  A a1;
+  A a2;
+};
+
+const A a1 = 1;
+const A a2 = 2;
+
+const B b[] = { {a1,a2}, {a1,a2} };
+
+const string CON_ONE = "string_1";
+const string CON_TWO = "string_2";
+
+struct StrOne
+{
+  string partOne;
+  string partTwo;
+};
+
+#define NUM_PARAMS 2
+
+const StrOne general[NUM_PARAMS] = {
+	{string(CON_ONE), string(CON_TWO)},
+	{string(CON_ONE), string(CON_TWO)}      
+};
+
+const string s[] = { CON_ONE, CON_TWO };
+
+int main()
+{
+  // string temp = general[0].partOne;
+  A i = b[0].a1;
+  return 0;
+}
