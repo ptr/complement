a1d3e55b4a31c9b54715a970e0e8f6540d32c700
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2006-07-11 09:00:17 +0000

    test for libsockios; restored after disk crash, fixed test for specific intarface listen and shutdown/close
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1218 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/test/libsockios/unit/client-mw.h
@@ -0,0 +1,31 @@
+// -*- C++ -*- Time-stamp: <05/12/01 20:29:26 ptr>
+
+/*
+ *
+ * Copyright (c) 2002, 2003
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 2.1
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ */
+
+#ifndef __ClientMassWrite_h
+#define __ClientMassWrite_h
+
+// Clients simulator
+
+class ClientMassWrite
+{
+  public:
+    static void client_proc();
+};
+
+#endif // __ClientMassWrite_h
