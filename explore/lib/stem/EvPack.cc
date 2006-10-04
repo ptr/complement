1a674d526d99ba6493c33cb410443e92e34dddf7
ptr <ptr@09cbc127-031f-0410-9985-c3ce8c6d183e>
2006-10-04 07:21:06 +0000

    erase target string in any case---length of string may be indeed 0
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1282 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/stem/EvPack.cc
+++ explore/lib/stem/EvPack.cc
@@ -1,23 +1,14 @@
-// -*- C++ -*- Time-stamp: <06/07/20 11:17:10 ptr>
+// -*- C++ -*- Time-stamp: <06/10/04 11:13:07 ptr>
 
 /*
- *
- * Copyright (c) 1997-1999, 2002, 2003, 2005
+ * Copyright (c) 1997-1999, 2002, 2003, 2005, 2006
  * Petr Ovtchenkov
  *
  * Copyright (c) 1999-2001
  * ParallelGraphics Ltd.
  *
- * Licensed under the Academic Free License version 2.1
+ * Licensed under the Academic Free License version 3.0
  * 
- * This material is provided "as is", with absolutely no warranty expressed
- * or implied. Any use is at your own risk.
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.
  */
 
 #include <config/feature.h>
@@ -38,8 +29,8 @@ void __pack_base::__net_unpack( istream& s, string& str )
   uint32_t sz;
   s.read( (char *)&sz, sizeof(uint32_t) );
   sz = from_net( sz );
+  str.erase();
   if ( sz > 0 ) {
-    str.erase();
     str.reserve( sz );
 #if defined(_MSC_VER) && (_MSC_VER < 1200)
     char ch;
@@ -64,11 +55,7 @@ void __pack_base::__net_pack( ostream& s, const string& str )
 #if !defined(__HP_aCC) || (__HP_aCC > 1) // linker problem, not compiler
   copy( str.begin(), str.end(), std::ostream_iterator<char,char,std::char_traits<char> >(s) );
 #else // Mmm, may be with __STL_DEBUG should be a bit different...
-// #  ifndef __STL_DEBUG
   copy( str.begin(), str.end(), ostream_iterator<char>(s) );
-// #  else
-//  __STD::__copy_aux(str.begin(), str.end(), __STD::ostream_iterator<char,char,__STD::char_traits<char> >(s), (char *)(0) );
-// #  endif
 #endif
 }
 
@@ -77,8 +64,8 @@ void __pack_base::__unpack( istream& s, string& str )
 {
   string::size_type sz;
   s.read( (char *)&sz, sizeof(sz) );
+  str.erase();
   if ( sz > 0 ) {
-    str.erase();
     str.reserve( sz );
 #if defined(_MSC_VER) && (_MSC_VER < 1200)
     char ch;
@@ -102,15 +89,7 @@ void __pack_base::__pack( ostream& s, const string& str )
 #if !defined(__HP_aCC) || (__HP_aCC > 1) // linker problem, not compiler
   std::copy( str.begin(), str.end(), std::ostream_iterator<char,char,std::char_traits<char> >(s) );
 #else // Mmm, may be with __STL_DEBUG should be a bit different...
-// #  ifndef __STL_DEBUG
   copy( str.begin(), str.end(), ostream_iterator<char>(s) );
-  // __STD::__copy_aux(str.begin(), str.end(), __STD::ostream_iterator<char,char,__STD::char_traits<char> >(s), (char *)(0) );
-// #  else
-//   __STD::__copy_aux(str.begin(), str.end(), __STD::ostream_iterator<char,char,__STD::char_traits<char> >(s), (char *)(0) );
-//  for ( string::size_type i = 0; i < str.size(); ++i ) {
-//    s << str[i];
-//  }
-// #  endif
 #endif
 }
 
