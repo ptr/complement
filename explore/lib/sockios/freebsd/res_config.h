1c9298357570002a3aa2f1d17b3d637af8117577
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2006-10-11 07:10:13 +0000

    fix typo
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1311 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/lib/sockios/freebsd/res_config.h
+++ explore/lib/sockios/freebsd/res_config.h
@@ -4,5 +4,5 @@
 #undef	USELOOPBACK	/* res_init() bind to localhost */
 #undef	SUNSECURITY	/* verify gethostbyaddr() calls - WE DONT NEED IT  */
 #define MULTI_PTRS_ARE_ALIASES 1 /* fold multiple PTR records into aliases */
-#define	CHICK_SRVR_EDDR 1 /* confirm that the server requested sent the reply */
+#define	CHECK_SRVR_ADDR 1 /* confirm that the server requested sent the reply */
 #define BIND_UPDATE 1	/* update support */
