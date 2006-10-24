992a6a96551c9628e04750c5973459fa3f15a9d0
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2006-10-24 06:41:35 +0000

    move timespec functions to time.cc; move sleep-like functions from Thread to xmt namespace in time.cc
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1346 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/include/mt/time.h
+++ explore/include/mt/time.h
@@ -1,40 +1,83 @@
-// -*- C++ -*- Time-stamp: <02/09/25 11:37:39 ptr>
+// -*- C++ -*- Time-stamp: <06/10/24 09:28:28 ptr>
 
 /*
- *
- * Copyright (c) 2002
+ * Copyright (c) 2002, 2006
  * Petr Ovtchenkov
  *
- * Licensed under the Academic Free License Version 1.0
+ * Licensed under the Academic Free License version 3.0
  *
- * This material is provided "as is", with absolutely no warranty expressed
- * or implied. Any use is at your own risk.
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.
  */
 
-#ifndef __time_h
-#define __time_h
-
-#ifdef __unix
-#  ifdef __HP_aCC
-#pragma VERSIONID "@(#)$Id$"
-#  else
-#ident "@(#)$Id$"
-#  endif
-#endif
+#ifndef __mt_time_h
+#define __mt_time_h
 
 #ifndef __config_feature_h
 #include <config/feature.h>
 #endif
 
+#ifdef WIN32
+# include <windows.h>
+# include <memory>
+# include <ctime>
+# include <limits>
+# pragma warning( disable : 4290)
+#endif // WIN32
+
 #include <string>
-#include <time.h>
+#include <ctime>
+
+#ifdef N_PLAT_NLM
+# include <sys/time.h> // timespec, timespec_t
+#endif
+
+#ifdef _WIN32
+extern "C" {
+
+typedef struct  timespec {              /* definition per POSIX.4 */
+        time_t          tv_sec;         /* seconds */
+        long            tv_nsec;        /* and nanoseconds */
+} timespec_t;
+
+} // extern "C"
+#endif // _WIN32
+
+#if defined(_WIN32) || defined(N_PLAT_NLM)
+extern "C" {
+
+typedef struct timespec timestruc_t;    /* definition per SVr4 */
+
+} // extern "C"
+#endif
 
 std::string calendar_time( time_t t );
 
-#endif // __time_h
+timespec operator +( const timespec& a, const timespec& b );
+timespec operator -( const timespec& a, const timespec& b );
+timespec operator /( const timespec& a, unsigned b );
+timespec operator /( const timespec& a, unsigned long b );
+
+// timespec& operator =( timespec& a, const timespec& b );
+timespec& operator +=( timespec& a, const timespec& b );
+timespec& operator -=( timespec& a, const timespec& b );
+timespec& operator /=( timespec& a, unsigned b );
+timespec& operator /=( timespec& a, unsigned long b );
+
+bool operator >( const timespec& a, const timespec& b );
+bool operator >=( const timespec& a, const timespec& b );
+bool operator <( const timespec& a, const timespec& b );
+bool operator <=( const timespec& a, const timespec& b );
+bool operator ==( const timespec& a, const timespec& b );
+bool operator !=( const timespec& a, const timespec& b );
+
+namespace xmt {
+
+// delay execution at least on time interval t
+__FIT_DECLSPEC void delay( timespec *interval, timespec *remain );
+// sleep at least up to time t
+__FIT_DECLSPEC void sleep( timespec *abstime, timespec *real_time );
+// get precise time
+__FIT_DECLSPEC void gettime( timespec *t );
+
+} // namespace xmt
+
+#endif // __mt_time_h
