84328cd2b06242ff66c3e2347504941855a4d2f2
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2006-11-24 10:15:25 +0000

    use int64_t to reduce comparisons
    git-svn-id: svn://localhost/complement/trunk@1404 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/include/mt/uid.h
+++ complement/explore/include/mt/uid.h
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <06/11/23 16:51:52 ptr>
+// -*- C++ -*- Time-stamp: <06/11/23 23:34:55 ptr>
 
 /*
  * Copyright (c) 2006
@@ -16,6 +16,7 @@
 #endif
 
 #include <string>
+// #include <algorithm>
 #include <stdint.h>
 
 namespace xmt {
@@ -25,6 +26,7 @@ struct uuid_type
   union {
     uint8_t  b[16];
     uint32_t i[4];
+    uint64_t l[2];
   } u;
 
   
@@ -33,19 +35,29 @@ struct uuid_type
 
   uuid_type( const uuid_type& uid )
   {
-    u.i[0] = uid.u.i[0];
-    u.i[1] = uid.u.i[1];
-    u.i[2] = uid.u.i[2];
-    u.i[3] = uid.u.i[3];
+    // u.i[0] = uid.u.i[0]; u.i[1] = uid.u.i[1];
+    // u.i[2] = uid.u.i[2]; u.i[3] = uid.u.i[3];
+    u.l[0] = uid.u.l[0]; u.l[1] = uid.u.l[1];
   }
 
 
-  bool operator ==( const uuid_type& uid )
-  { return u.i[0] == uid.u.i[0] && u.i[1] == uid.u.i[1] && u.i[2] == uid.u.i[2] && u.i[3] == uid.u.i[3]; }
+    bool operator ==( const uuid_type& uid ) const
+      {
+        // return u.i[0] == uid.u.i[0] && u.i[1] == uid.u.i[1] && u.i[2] == uid.u.i[2] && u.i[3] == uid.u.i[3];
+        return u.l[0] == uid.u.l[0] && u.l[1] == uid.u.l[1];
+      }
+    
+    bool operator !=( const uuid_type& uid ) const
+      {
+        // return u.i[0] != uid.u.i[0] || u.i[1] != uid.u.i[1] || u.i[2] != uid.u.i[2] || u.i[3] != uid.u.i[3];
+        return u.l[0] != uid.u.l[0] || u.l[1] != uid.u.l[1];
+      }
 
-  bool operator !=( const uuid_type& uid )
-  { return u.i[0] != uid.u.i[0] || u.i[1] != uid.u.i[1] || u.i[2] != uid.u.i[2] || u.i[3] != uid.u.i[3]; }
-  
+    bool operator <( const uuid_type& uid ) const
+      {
+        // return std::lexicographical_compare( u.i, u.i + 4, uid.u.i, uid.u.i + 4 );
+        return u.l < uid.u.l ? true : u.l > uid.u.l ? false : (u.l[1] < uid.u.l[1]);
+      }
 };
 
 const std::string& hostid_str();
