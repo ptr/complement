8405d93a793891d60d6a60f3f7335654ca6aa858
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-02-01 17:23:03 +0000

    add 'signal' to thr_mgr---send signal to all good threads in pool; use fact, that thread's join in Thread destructor now.
    join in Thread destructor;
    
    timespec multiplication on double added
    
    
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1502 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/lib/mt/thr_mgr.cc
+++ complement/explore/lib/mt/thr_mgr.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <07/02/01 11:18:04 ptr>
+// -*- C++ -*- Time-stamp: <07/02/01 20:17:50 ptr>
 
 /*
  * Copyright (c) 1997-1999, 2002, 2005-2007
@@ -15,7 +15,11 @@
 #include <algorithm>
 #include <functional>
 
+// #include <iostream>
+
 namespace xmt {
+
+// int _supercount = 0;
 
 using namespace std;
 
@@ -24,6 +28,13 @@ struct bad_thread :
 {
     bool operator()(const Thread *__x) const
       { return !__x->good(); }
+};
+
+struct good_thread :
+    public unary_function<Thread *,bool>
+{
+    bool operator()(const Thread *__x) const
+      { return (__x != 0) && (__x->good()); }
 };
 
 struct rm_if_bad_thread :
@@ -37,12 +48,25 @@ bool rm_if_bad_thread::operator()(Thread *__x)
   if ( __x == 0 ) {
     return true;
   }
-  if ( !__x->good() ) {
-    __x->join();
+  if ( __x->bad() ) {
+    // --_supercount;
     delete __x;
     return true;
   }
   return false;
+}
+
+struct thread_signal :
+    public binary_function<Thread *,int,void>
+{
+    void operator()(const Thread *__x, int sig ) const;
+};
+
+void thread_signal::operator()(const Thread *__x, int sig ) const
+{
+  if ( __x != 0 ) {
+    const_cast<Thread *>(__x)->kill( sig );
+  }
 }
 
 __FIT_DECLSPEC ThreadMgr::~ThreadMgr()
@@ -58,12 +82,17 @@ __FIT_DECLSPEC void ThreadMgr::join()
   // xmt::block_signal( SIGCHLD );
   // xmt::block_signal( SIGPOLL );
 
-  Locker lk( _lock );
+  _lock.lock();
   _M_c.erase( remove_if( _M_c.begin(), _M_c.end(), rm_if_bad_thread() ), _M_c.end() );
   while ( !_M_c.empty() ) {
-    xmt::delay( xmt::timespec(0,100000000) );
+    _lock.unlock();
+    xmt::delay( xmt::timespec(0,50000000) );
+    _lock.lock();
     _M_c.erase( remove_if( _M_c.begin(), _M_c.end(), rm_if_bad_thread() ), _M_c.end() );
+    // cerr << "### " << _supercount << " " << _M_c.size() << endl;
   }
+  // _supercount = 0;
+  _lock.unlock();
 }
 
 __FIT_DECLSPEC
@@ -72,6 +101,7 @@ void ThreadMgr::launch( Thread::entrance_type entrance, const void *p, size_t ps
   Locker lk( _lock );
   _M_c.erase( remove_if( _M_c.begin(), _M_c.end(), rm_if_bad_thread() ), _M_c.end() );
   _M_c.push_back( new Thread( entrance, p, psz, flags, stack_sz ) );
+  // ++_supercount;
 }
 
 __FIT_DECLSPEC
@@ -84,8 +114,17 @@ void ThreadMgr::garbage_collector()
 ThreadMgr::container_type::size_type ThreadMgr::size()
 {
   Locker lk( _lock );
-  _M_c.erase( remove_if( _M_c.begin(), _M_c.end(), rm_if_bad_thread() ), _M_c.end() );
-  return _M_c.size();
+  // ThreadMgr::container_type::size_type sz = count_if( _M_c.begin(), _M_c.end(), good_thread() );
+  // cerr << "Sz: " << sz << endl;
+  
+  return count_if( _M_c.begin(), _M_c.end(), good_thread() );
+}
+
+__FIT_DECLSPEC void ThreadMgr::signal( int sig )
+{
+  // cerr << "Signal!" << endl;
+  Locker lk( _lock );
+  for_each( _M_c.begin(), _M_c.end(), bind2nd( thread_signal(), sig ) );
 }
 
 } // namespace xmt
