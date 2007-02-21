4485694f7d5b9481fd72ba0bcd2f2f0140001ab7
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-02-21 17:57:04 +0000

    more funcs
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk/complement@1532 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/inquiry/shades/juniper/str_interposer.c
+++ explore/inquiry/shades/juniper/str_interposer.c
@@ -2,17 +2,18 @@
  *
  */
 
-/* Example of a library interposer: interpose on 
- * malloc().
- * Build and use this interposer as following:
- * cc -o malloc_interposer.so -G -Kpic malloc_interposer.c
- * setenv LD_PRELOAD $cwd/malloc_interposer.so
+/* library interposer: interpose on
+ * str...().
+ * export LD_PRELOAD=$cwd/libtest.so.0.0
  * run the app
- * unsetenv LD_PRELOAD
+ * unset LD_PRELOAD
  */
 
 #include <stdio.h>
 #include <dlfcn.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 
 #if 0
 void *malloc(size_t size)
@@ -30,7 +31,7 @@ char *strcpy( char *s1, const char *s2 )
 {
   static void * (*func)();
 
-  if(!func)
+  if (!func)
     func = (void *(*)()) dlsym(RTLD_NEXT, "strcpy");
 
   printf( "strcpy: '%s'\n", s2 );
@@ -42,7 +43,7 @@ char *strdup(const char *s1)
 {
   static void * (*func)();
 
-  if(!func)
+  if (!func)
     func = (void *(*)()) dlsym(RTLD_NEXT, "strdup");
 
   printf( "strdup: '%s'\n", s1 );
@@ -55,7 +56,7 @@ char *strncat(char *s1, const char *s2, size_t n)
   static void * (*func)();
   char *r;
 
-  if(!func)
+  if (!func)
     func = (void *(*)()) dlsym(RTLD_NEXT, "strncat");
 
   r = func( s1, s2, n );
@@ -68,7 +69,7 @@ char *strtok(char *s1, const char *s2)
 {
   static void * (*func)();
 
-  if(!func)
+  if (!func)
     func = (void *(*)()) dlsym(RTLD_NEXT, "strtok");
 
   printf( "strtok: '%s'\n", s1 );
@@ -76,3 +77,50 @@ char *strtok(char *s1, const char *s2)
   return func( s1, s2 );
 }
 
+ssize_t sendto(int socket, const void *message, size_t length, int flags, const struct sockaddr *dest_addr, socklen_t dest_len)
+{
+  static ssize_t (*func)();
+  int i;
+
+  if (!func)
+    func = (ssize_t (*)()) dlsym(RTLD_NEXT, "sendto");
+
+  printf( "sendto: (%d) '", length );
+  for ( i = 0; i < length; ++i ) {
+    printf( "%c", (const char *)message + i );
+  }
+  printf( "'\n" );
+  return func( socket, message, length, flags, dest_addr, dest_len );
+}
+
+ssize_t recvfrom(int socket, void *buffer, size_t length, int flags, struct sockaddr *address, socklen_t *address_len)
+{
+  static ssize_t (*func)();
+  int i;
+  ssize_t s;
+
+  if (!func)
+    func = (ssize_t (*)()) dlsym(RTLD_NEXT, "recvfrom");
+
+  s = func( socket, buffer, length, flags, address, address_len );
+  printf( "recvfrom: (%d) '", length );
+  for ( i = 0; i < length; ++i ) {
+    printf( "%c", (const char *)buffer + i );
+  }
+  printf( "'\n" );
+  return s;
+}
+
+int connect(int socket, const struct sockaddr *address, socklen_t address_len)
+{
+  static int (*func)();
+
+  if (!func)
+    func = (int (*)()) dlsym(RTLD_NEXT, "connect");
+
+  printf( "connect: " );
+  printf( "%x", *(((unsigned *)&((struct sockaddr_in *)address)->sin_addr)) );
+  printf( ":%d\n", ntohs(((struct sockaddr_in *)address)->sin_port) );
+
+  return func( socket, address, address_len );
+}
