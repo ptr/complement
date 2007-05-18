94cedca4f60f86bbffc3a6317dd24e86578231db
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-05-18 15:34:06 +0000

    development
    git-svn-id: svn://localhost/complement/trunk@1565 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/test/virtual_time/vtime_main.cc
+++ complement/explore/test/virtual_time/vtime_main.cc
@@ -20,6 +20,13 @@ int main()
   Proc r1( 102 );
   Proc r3( 103 );
 
+  stem::Event_base<VTmess> mess( MESS );
+  
+  mess.dest( 101 );
+  mess.value().mess = "Hello!";
+
+  m1.Send( mess );
+
   cnd.wait();
 
   return 0;
