fb954214c50b990c0baf483ac7a3054c7467766d
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-05-18 07:12:34 +0000

    development
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1563 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/test/virtual_time/vtime.h
+++ complement/explore/test/virtual_time/vtime.h
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <07/03/06 19:23:37 ptr>
+// -*- C++ -*- Time-stamp: <07/05/18 00:26:00 ptr>
 
 #ifndef __vtime_h
 #define __vtime_h
@@ -19,13 +19,18 @@ typedef unsigned vtime_unit_type;
 typedef std::pair<stem::addr_type, vtime_unit_type> vtime_proc_type;
 typedef std::list<vtime_proc_type> vtime_type;
 
+inline bool uorder( const vtime_proc_type& l, const vtime_proc_type& r )
+{
+  return l.first < r.first;
+}
+
 inline bool operator <( const vtime_proc_type& l, const vtime_proc_type& r )
 {
   if ( l.first == r.first ) {
     return l.second < r.second;
   }
 
-  throw std::invalid_argument( "uncomparable" );
+  throw std::invalid_argument( "uncomparable vtime" );
 }
 
 inline bool operator <=( const vtime_proc_type& l, const vtime_proc_type& r )
@@ -34,7 +39,7 @@ inline bool operator <=( const vtime_proc_type& l, const vtime_proc_type& r )
     return l.second <= r.second;
   }
 
-  throw std::invalid_argument( "uncomparable" );
+  throw std::invalid_argument( "uncomparable vtime" );
 }
 
 inline bool operator ==( const vtime_proc_type& l, const vtime_proc_type& r )
@@ -43,9 +48,48 @@ inline bool operator ==( const vtime_proc_type& l, const vtime_proc_type& r )
     return l.second == r.second;
   }
 
-  throw std::invalid_argument( "uncomparable" );
+  throw std::invalid_argument( "uncomparable vtime" );
 }
 
+
+#if 0
+bool operator <=( const vtime_type& l, const vtime_type& r )
+{
+  if ( l.size() == 0 ) { // 0 always less or equal of anything
+    return true;
+  }
+
+  bool result = true;
+  vtime_type::const_iterator i = l.begin();
+  vtime_type::const_iterator j = r.begin();
+
+  while ( j->first < i->first && j != r.end() ) {
+    ++j;
+  }
+
+  if ( j == r.end() ) { // note, that i != l.end() here!
+    return false;
+  }
+
+  for ( ; i != l.end() && j != r.end(); ) {
+    if ( i->second > j->second ) {
+    }
+    
+    if ( i->first < j->first ) {
+      ++i;
+    } else if ( i->first == j->first ) {
+      if ( i->second > j->second ) {
+        return false;
+      }
+    } else {
+      ++j;
+    }
+  }
+}
+#endif
+
+
+vtime_type operator -( const vtime_type& l, const vtime_type& r );
 
 struct vtime :
     public stem::__pack_base
