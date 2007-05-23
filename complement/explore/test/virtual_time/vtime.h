3b8eea00c45e9fb4c95f9b8f3effc813ba44df20
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-05-23 15:26:04 +0000

    development
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1569 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/test/virtual_time/vtime.h
+++ complement/explore/test/virtual_time/vtime.h
@@ -17,9 +17,10 @@
 
 namespace vt {
 
+typedef stem::addr_type oid_type;
 typedef unsigned vtime_unit_type;
 typedef uint32_t group_type;
-typedef std::pair<stem::addr_type, vtime_unit_type> vtime_proc_type;
+typedef std::pair<oid_type, vtime_unit_type> vtime_proc_type;
 typedef std::list<vtime_proc_type> vtime_type;
 
 inline bool uorder( const vtime_proc_type& l, const vtime_proc_type& r )
@@ -57,6 +58,7 @@ inline bool operator ==( const vtime_proc_type& l, const vtime_proc_type& r )
 bool operator <=( const vtime_type& l, const vtime_type& r );
 vtime_type operator -( const vtime_type& l, const vtime_type& r );
 vtime_type operator +( const vtime_type& l, const vtime_type& r );
+vtime_type& operator +=( vtime_type& l, const vtime_type& r );
 
 vtime_type max( const vtime_type& l, const vtime_type& r );
 
@@ -73,10 +75,41 @@ struct vtime :
   vtime( const vtime& _vt ) :
     vt( _vt.vt.begin(), _vt.vt.end() )
     { }
+  vtime( const vtime_type& _vt ) :
+    vt( _vt.begin(), _vt.end() )
+    { }
 
   vtime& operator =( const vtime& _vt )
-  { vt.clear(); std::copy( _vt.vt.begin(), _vt.vt.end(), std::back_insert_iterator<vtime_type>(vt) ); }
+    {
+      vt.clear();
+      std::copy( _vt.vt.begin(), _vt.vt.end(), std::back_insert_iterator<vtime_type>(vt) );
+    }
 
+  bool operator ==( const vtime& r ) const
+    { return vt == r.vt; }
+
+  bool operator <=( const vtime& r ) const
+    { return vt <= r.vt; }
+
+  vtime operator -( const vtime& r ) const
+    { return vtime( vt - r.vt ); }
+
+  vtime operator +( const vtime& r ) const
+    { return vtime( vt + r.vt ); }
+
+  vtime& operator +=( const vtime_type& t )
+  {
+    vt += t;
+    return *this;
+  }
+  vtime& operator +=( const vtime& t )
+  {
+    vt += t.vt;
+    return *this;
+  }
+  
+  vtime& operator +=( const vtime_proc_type& );
+    
   vtime_type vt;
 };
 
@@ -98,7 +131,12 @@ struct gvtime :
     { }
 
   gvtime& operator =( const gvtime& _gvt )
-  { gvt.clear(); std::copy( _gvt.gvt.begin(), _gvt.gvt.end(), std::back_insert_iterator<gvtime_type>(gvt) ); }
+    {
+      gvt.clear();
+      std::copy( _gvt.gvt.begin(), _gvt.gvt.end(), std::back_insert_iterator<gvtime_type>(gvt) );
+    }
+
+  gvtime& operator +=( const vtime_group_type& );
 
   gvtime_type gvt;
 };
@@ -119,6 +157,7 @@ struct VTmess :
     { }
 
   gvtime gvt;
+  group_type grp;
   std::string mess;
 };
 
@@ -154,5 +193,14 @@ class Proc :
 #define MESS 0x300
 
 } // namespace vt
+
+namespace std {
+
+ostream& operator <<( ostream&, const vt::vtime_proc_type& );
+ostream& operator <<( ostream&, const vt::vtime_type& );
+ostream& operator <<( ostream&, const vt::vtime_group_type& );
+ostream& operator <<( ostream&, const vt::gvtime_type& );
+
+} // namespace std
 
 #endif // __vtime_h
--- complement/explore/test/virtual_time/vtime_main.cc
+++ complement/explore/test/virtual_time/vtime_main.cc
@@ -23,7 +23,15 @@ int main()
   stem::Event_base<VTmess> mess( MESS );
   
   mess.dest( 101 );
+
+  vtime_group_type gvt;
+
+  gvt.first = 0; // group
+  gvt.second += make_pair( 101, 1 );
+
   mess.value().mess = "Hello!";
+  mess.value().grp = 0;
+  mess.value().gvt += gvt;
 
   m1.Send( mess );
 
