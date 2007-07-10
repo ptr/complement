5c77fddcdaee54aae9ed418b7906a3f2a7cdb169
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-07-10 17:12:18 +0000

    function and class function work
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk/complement@1601 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/app/exam/suite.cc
+++ explore/app/exam/suite.cc
@@ -1,8 +1,11 @@
-// -*- C++ -*- Time-stamp: <07/07/06 09:55:44 ptr>
+// -*- C++ -*- Time-stamp: <07/07/08 23:39:10 ptr>
 
 #include "suite.h"
 #include <boost/graph/breadth_first_search.hpp>
 #include <stack>
+
+#include <cstdio>
+#include <iostream>
 
 namespace exam {
 
@@ -39,7 +42,14 @@ test_suite::test_suite() :
   _test[root] = 0;
 }
 
-void test_suite::girdle( int start )
+test_suite::~test_suite()
+{
+  for ( test_case_map_type::iterator i = _test.begin(); i != _test.end(); ++i ) {
+    delete i->second;
+  }
+}
+
+void test_suite::girdle()
 {
   stack<vertex_t> buffer;
   list<vertex_t> v;
@@ -58,6 +68,26 @@ void test_suite::girdle( int start )
   }
 }
 
+void test_suite::girdle( test_suite::test_case_type start )
+{
+  stack<vertex_t> buffer;
+  list<vertex_t> v;
+  breadth_first_visit( g, start, buffer,
+                       make_bfs_visitor(record_vertexes(v,on_discover_vertex())),
+                       color );
+
+  v.pop_front(); // remove root, it empty
+
+  for ( list<vertex_t>::const_iterator i = v.begin(); i != v.end(); ++i ) {
+    try {
+      (*_test[*i])();
+    }
+    catch ( ... ) {
+    }
+  }
+}
+
+
 test_suite::test_case_type test_suite::add( test_suite::func_type f )
 {
   vertex_t v = add_vertex( white_color, g);
@@ -74,6 +104,40 @@ test_suite::test_case_type test_suite::add( test_suite::func_type f, test_suite:
   _test[v] = detail::make_test_case( detail::call( f ) );
 
   return v;
+}
+
+int test_suite::flags()
+{
+  return _flags;
+}
+
+bool test_suite::is_trace()
+{
+  return (_flags & trace) != 0;
+}
+
+void _report0( const char *file, int line, bool cnd, const char *expr )
+{
+  std::cerr << file << ":" << line << ": " << (cnd ? "pass" : "fail" ) << ": " << expr
+            << std::endl;
+}
+
+void _report1( const char *file, int line, bool cnd, const char *expr )
+{
+  printf( "%s:%d: %s: %s\n", file, line, (cnd ? "pass" : "fail"), expr );
+}
+
+void _report2( const char *file, int line, bool cnd, const char *expr )
+{
+  fprintf( stderr, "%s:%d: %s: %s\n", file, line, (cnd ? "pass" : "fail"), expr );
+}
+
+int test_suite::_flags = 0;
+void (*test_suite::_report)( const char *, int, bool, const char * ) = _report0;
+
+void test_suite::report( const char *file, int line, bool cnd, const char *expr )
+{
+  (*test_suite::_report)( file, line, cnd, expr );
 }
 
 } // namespace exam
