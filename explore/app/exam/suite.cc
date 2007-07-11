c117ca928bb0f019f7ee5f2a578b71a895b8e07f
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-07-11 07:35:11 +0000

    test functions return int, conditional run (from dependency)
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk/complement@1602 09cbc127-031f-0410-9985-c3ce8c6d183e

--- explore/app/exam/suite.cc
+++ explore/app/exam/suite.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <07/07/08 23:39:10 ptr>
+// -*- C++ -*- Time-stamp: <07/07/11 11:10:45 ptr>
 
 #include "suite.h"
 #include <boost/graph/breadth_first_search.hpp>
@@ -11,61 +11,87 @@ namespace exam {
 
 using namespace std;
 using namespace boost;
+using namespace detail;
 
+namespace detail {
 
-template <class VertexList, class Tag>
+template <class Tag>
 struct vertex_recorder :
-        public base_visitor<vertex_recorder<VertexList,Tag> >
+        public base_visitor<vertex_recorder<Tag> >
 {
     typedef Tag event_filter;
 
-    vertex_recorder(VertexList& pa) :
-        m_vertex(pa)
+    vertex_recorder(test_suite& ts) :
+        _suite(ts)
       { }
 
     template <class Vertex, class Graph>
     void operator()(Vertex v, const Graph& g)
-      { m_vertex.push_back( v ); }
+      { _suite.run_test_case( v ); }
 
-    VertexList& m_vertex;
+    test_suite& _suite;
 };
 
-template <class VertexList, class Tag>
-vertex_recorder<VertexList, Tag> record_vertexes(VertexList& pa, Tag)
-{ return vertex_recorder<VertexList, Tag>(pa); }
+template <class Tag>
+vertex_recorder<Tag> record_vertexes(test_suite& ts, Tag)
+{ return vertex_recorder<Tag>(ts); }
+
+template <class Tag>
+struct skip_recorder :
+        public base_visitor<skip_recorder<Tag> >
+{
+    typedef Tag event_filter;
+
+    skip_recorder(test_suite& ts) :
+        _suite(ts)
+      { }
+
+    template <class Edge, class Graph>
+    void operator()(Edge e, const Graph& g)
+      { 
+        // typename graph_traits<Graph>::vertex_descriptor u = boost::source( e, g );
+        // typename graph_traits<Graph>::vertex_descriptor v = boost::target( e, g );
+        _suite.check_test_case( boost::source( e, g ), boost::target( e, g ) );
+      }
+
+    test_suite& _suite;
+};
+
+template <class Tag>
+skip_recorder<Tag> record_skip(test_suite& ts, Tag)
+{ return skip_recorder<Tag>(ts); }
+
+int _root_func()
+{
+  return 0;
+}
+
+} // namespace detail
 
 test_suite::test_suite() :
    root( add_vertex( white_color, g ) )
 {
   color = get( vertex_color, g );
   testcase = get( vertex_testcase, g );
-  _test[root] = 0;
+  _test[root].tc = detail::make_test_case( detail::call( _root_func ) );
+  _test[root].state = 0;
 }
 
 test_suite::~test_suite()
 {
   for ( test_case_map_type::iterator i = _test.begin(); i != _test.end(); ++i ) {
-    delete i->second;
+    delete i->second.tc;
   }
 }
 
 void test_suite::girdle()
 {
   stack<vertex_t> buffer;
-  list<vertex_t> v;
   breadth_first_visit( g, root, buffer,
-                       make_bfs_visitor(record_vertexes(v,on_discover_vertex())),
+                       make_bfs_visitor(
+                         make_pair( record_vertexes(*this,on_discover_vertex()),
+                                    record_skip(*this,on_examine_edge()) ) ),
                        color );
-
-  v.pop_front(); // remove root, it empty
-
-  for ( list<vertex_t>::const_iterator i = v.begin(); i != v.end(); ++i ) {
-    try {
-      (*_test[*i])();
-    }
-    catch ( ... ) {
-    }
-  }
 }
 
 void test_suite::girdle( test_suite::test_case_type start )
@@ -73,18 +99,10 @@ void test_suite::girdle( test_suite::test_case_type start )
   stack<vertex_t> buffer;
   list<vertex_t> v;
   breadth_first_visit( g, start, buffer,
-                       make_bfs_visitor(record_vertexes(v,on_discover_vertex())),
+                       make_bfs_visitor(
+                         make_pair( record_vertexes(*this,on_discover_vertex()),
+                                    record_skip(*this,on_examine_edge()) ) ),
                        color );
-
-  v.pop_front(); // remove root, it empty
-
-  for ( list<vertex_t>::const_iterator i = v.begin(); i != v.end(); ++i ) {
-    try {
-      (*_test[*i])();
-    }
-    catch ( ... ) {
-    }
-  }
 }
 
 
@@ -92,7 +110,8 @@ test_suite::test_case_type test_suite::add( test_suite::func_type f )
 {
   vertex_t v = add_vertex( white_color, g);
   add_edge( root, v, g );
-  _test[v] = detail::make_test_case( detail::call( f ) );
+  _test[v].tc = detail::make_test_case( detail::call( f ) );
+  _test[v].state = 0;
 
   return v;
 }
@@ -101,7 +120,8 @@ test_suite::test_case_type test_suite::add( test_suite::func_type f, test_suite:
 {
   vertex_t v = add_vertex( white_color, g);
   add_edge( depends, v, g );
-  _test[v] = detail::make_test_case( detail::call( f ) );
+  _test[v].tc = detail::make_test_case( detail::call( f ) );
+  _test[v].state = 0;
 
   return v;
 }
@@ -138,6 +158,27 @@ void (*test_suite::_report)( const char *, int, bool, const char * ) = _report0;
 void test_suite::report( const char *file, int line, bool cnd, const char *expr )
 {
   (*test_suite::_report)( file, line, cnd, expr );
+}
+
+void test_suite::run_test_case( test_suite::vertex_t v )
+{
+  try {
+    if ( _test[v].state == 0 ) {
+      if ( (*_test[v].tc)() != 0 ) {
+        _test[v].state = fail;
+      }
+    }
+  }
+  catch ( ... ) {
+    _test[v].state = fail;
+  }
+}
+
+void test_suite::check_test_case( test_suite::vertex_t u, test_suite::vertex_t v )
+{
+  if ( _test[u].state != 0 ) {
+    _test[v].state = skip;
+  }
 }
 
 } // namespace exam
