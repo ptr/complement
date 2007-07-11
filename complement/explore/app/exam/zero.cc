4bd780914123c99fdfd4c4e74643c834206bd556
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-07-11 19:56:54 +0000

    test suite messages, initial
    git-svn-id: svn://localhost/complement/trunk@1605 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/app/exam/zero.cc
+++ complement/explore/app/exam/zero.cc
@@ -9,6 +9,13 @@ int func()
   return 0;
 }
 
+int func2()
+{
+  EXAM_CHECK(true);
+
+  return 0;
+}
+
 class test_x
 {
   public:
@@ -24,14 +31,18 @@ class test_x
 
 int main( int argc, char **argv )
 {
-  exam::test_suite t;
+  exam::test_suite t( "exam level 0" );
 
   test_x tx;
 
-  t.add( func );
-  t.add( &test_x::f, tx );
+  t.add( func, "simple function" );
+  t.add( &test_x::f, tx, "member function" );
 
-  t.girdle( 0 );
+  t.add( func, "simple function, depends",
+         t.add( &test_x::f, tx, "member function, depends",
+                t.add( func2, "simple good function" ) ) );
+
+  t.girdle();
 
   return 0;
 }
