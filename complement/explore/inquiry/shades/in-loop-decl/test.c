d9883c56692d671c28338a60a8b5a5124808fb0e
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-07-12 07:26:45 +0000

    in-loop declaration influence only position on stack, but not more
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1608 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ complement/explore/inquiry/shades/in-loop-decl/test.c
@@ -0,0 +1,20 @@
+int q( int b )
+{
+  int a;
+  int i = 0;
+  while ( i < 10 ) {
+    a = b;
+    ++i;
+  }
+  return 0;
+}
+
+int qq( int b )
+{
+  int i = 0;
+  while ( i < 10 ) {
+    int a = b;
+    ++i;
+  }
+  return 0;
+}
