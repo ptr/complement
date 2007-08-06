3217d062bf04e80830cc86162c91dcae017b90eb
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-08-06 07:01:16 +0000

    more traits; established unit tests for type_traits workaround
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk/complement@1675 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ explore/lib/misc/ut/misc_test.h
@@ -0,0 +1,24 @@
+// -*- C++ -*- Time-stamp: <07/07/16 21:01:43 ptr>
+
+/*
+ * Copyright (c) 2007
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 3.0
+ *
+ */
+
+#ifndef __MISC_TEST_H
+#define __MISC_TEST_H
+
+#define FIT_EXAM
+
+#include <exam/suite.h>
+
+class misc_test
+{
+  public:
+    int EXAM_DECL(type_traits);
+};
+
+#endif // __MISC_TEST_H
---
+++ explore/lib/misc/ut/misc_test_suite.cc
@@ -0,0 +1,26 @@
+// -*- C++ -*- Time-stamp: <07/07/17 10:20:08 ptr>
+
+/*
+ * Copyright (c) 2007
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 3.0
+ *
+ */
+
+#include "misc_test_suite.h"
+#include "misc_test.h"
+
+#include <config/feature.h>
+
+int EXAM_IMPL(misc_test_suite)
+{
+  exam::test_suite t( "libmisc? test" );
+  misc_test test;
+
+  exam::test_suite::test_case_type tc[3];
+
+  tc[0] = t.add( &misc_test::type_traits, test, "misc_test::type_traits" );
+
+  return t.girdle();
+};
