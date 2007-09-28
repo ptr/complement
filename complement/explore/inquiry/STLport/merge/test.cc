679c21db0d63cbb9097950e7fbcacc9d2f3a7513
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-09-28 11:03:56 +0000

    problem in stack_allocator?
    git-svn-id: svn://localhost/complement/trunk@1751 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/inquiry/STLport/merge/test.cc
+++ complement/explore/inquiry/STLport/merge/test.cc
@@ -1,21 +1,23 @@
 #include <list>
 #include <iostream>
 
+#include "stack_allocator.h"
+
 using namespace std;
 
 int main()
 {
-  // char buf1[1024];
-  // StackAllocator<int> stack1(buf1, buf1 + sizeof(buf1));
+  char buf1[1024];
+  StackAllocator<int> stack1(buf1, buf1 + sizeof(buf1));
 
-  // char buf2[1024];
-  // StackAllocator<int> stack2(buf2, buf2 + sizeof(buf2));
+  char buf2[1024];
+  StackAllocator<int> stack2(buf2, buf2 + sizeof(buf2));
 
-  // typedef list<int, StackAllocator<int> > ListInt;
-  typedef list<int> ListInt;
+  typedef list<int, StackAllocator<int> > ListInt;
+  // typedef list<int> ListInt;
 
-  ListInt lint1(10, 0 /* , stack1 */ );
-  ListInt lint2(10, 1 /* , stack2 */ );
+  ListInt lint1(10, 0, stack1 );
+  ListInt lint2(10, 1, stack2 );
 
   // ListInt lintref(stack2);
   // lintref.insert(lintref.begin(), 10, 1);
