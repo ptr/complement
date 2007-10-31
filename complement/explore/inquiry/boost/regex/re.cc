d24e029938042f2fee77277c4391f3ea64332ca3
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2007-10-31 17:38:28 +0000

    see on captures
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1780 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/inquiry/boost/regex/re.cc
+++ complement/explore/inquiry/boost/regex/re.cc
@@ -8,6 +8,7 @@ using namespace boost;
 
 int main()
 {
+#if 0
   string s( "12345678" );
   const regex re1( "^[0-9]+" );
   const regex re2( "\\d" );
@@ -29,6 +30,25 @@ int main()
     cout << m.str() << endl;
     b = m[0].second;
   }
+#endif
+#if 1
+  string s( "L=DGhFBHBqSWNZWQN/XHlmc2FwfVN/C1VsHTg0GlsAKBg1FD9VGgRYBh8COC0xDiMjTlxJOSk/cQ4ZIAN/Lm5+CQ==.1193829498.1711.233703.f3ced884c7fb604c9a53e562ff921344; path=/; domain=.yandex.ru; expires=Tuesday, 29-Jan-08 11:18:18 GMT" );
+  // boost::regex cookie_re( "^(?:(\\w+)=([^;]*?)(?:;[[:space:]]+))*?(?:(\\w+)=([^;]*?))$" );
+  boost::regex cookie_re( "(?:(\\w+)=([^;]*)(?:;\\s+)?)*" );
+  boost::smatch ma;
+  if ( regex_search( s, ma, cookie_re, boost::match_extra ) ) {
+    for ( int i = 1; i < ma.size(); ++i ) {
+      cout << ma[i] << endl;
+#ifdef BOOST_REGEX_MATCH_EXTRA
+      cout << "@ ";
+      for ( int j = 0; j < ma.captures(i).size(); ++j ) {
+        cout << ma.captures(i)[j] << "%";
+      }
+      cout << endl;
+#endif
+    }
+  }
+#endif
   
   return 0;
 }
