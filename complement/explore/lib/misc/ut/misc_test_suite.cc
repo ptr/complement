96d3d12377b5ca791a786a8fdfdad679b79ff159
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2008-02-27 14:18:59 +0000

    a lot of tests for type_traits
    git-svn-id: svn://localhost/complement/trunk@1820 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/lib/misc/ut/misc_test_suite.cc
+++ complement/explore/lib/misc/ut/misc_test_suite.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <07/07/17 10:20:08 ptr>
+// -*- C++ -*- Time-stamp: <07/12/02 18:57:27 ptr>
 
 /*
  * Copyright (c) 2007
@@ -18,10 +18,41 @@ int EXAM_IMPL(misc_test_suite)
   exam::test_suite t( "libmisc? test" );
   misc_test test;
 
-  exam::test_suite::test_case_type tc[3];
+  exam::test_suite::test_case_type tc[10];
 
-  t.add( &misc_test::type_traits_is_empty, test, "misc_test::type_traits_is_empty",
-    t.add( &misc_test::type_traits_internals, test, "misc_test::type_traits_internals" ) );
+  tc[0] = t.add( &misc_test::type_traits_internals, test, "traits_internals" );
+
+  t.add( &misc_test::type_traits_is_void, test, "is_void", tc[0] );
+  tc[1] = t.add( &misc_test::type_traits_is_integral, test, "is_integral", tc[0] );
+  tc[2] = t.add( &misc_test::type_traits_is_floating_point, test, "is_floating_point", tc[0] );
+  t.add( &misc_test::type_traits_is_array, test, "is_array", tc[0] );
+  t.add( &misc_test::type_traits_is_pointer, test, "is_pointer", tc[0] );
+  t.add( &misc_test::type_traits_is_lvalue_reference, test, "is_lvalue_reference", tc[0] );
+  // t.add( &misc_test::type_traits_is_rvalue_reference, test, "is_rvalue_reference", tc[0] );
+
+  t.add( &misc_test::type_traits_is_member_object_pointer, test, "is_member_object_pointer", tc[0] );
+  t.add( &misc_test::type_traits_is_member_function_pointer, test, "is_member_function_pointer", tc[0] );
+
+  t.add( &misc_test::type_traits_is_enum, test, "is_enum", tc[0] );
+  t.add( &misc_test::type_traits_is_function, test, "is_function", tc[0] );
+
+  // [20.4.4.2]
+  t.add( &misc_test::type_traits_is_reference, test, "is_reference", tc[0] );
+  tc[3] = t.add( &misc_test::type_traits_is_arithmetic, test, "is_arithmetic", tc + 1, tc + 3 );
+  t.add( &misc_test::type_traits_is_fundamental, test, "is_fundamental", tc[3] );
+
+  t.add( &misc_test::type_traits_is_object, test, "is_object", tc[0] );
+  t.add( &misc_test::type_traits_is_scalar, test, "is_scalar", tc[0] );
+  t.add( &misc_test::type_traits_is_compound, test, "is_compound", tc[0] );
+  t.add( &misc_test::type_traits_is_member_pointer, test, "is_member_pointer", tc[0] );
+
+  // [20.4.4.3]
+  t.add( &misc_test::type_traits_is_const, test, "is_const", tc[0] );
+  t.add( &misc_test::type_traits_is_volatile, test, "is_volatile", tc[0] );
+  // t.add( &misc_test::type_traits_is_trivial, test, "is_trivial", tc[0] );
+  // t.add( &misc_test::type_traits_is_standard_layout, test, "is_standard_layout", tc[0] );
+  t.add( &misc_test::type_traits_is_pod, test, "is_pod", tc[0] );
+  t.add( &misc_test::type_traits_is_empty, test, "is_empty", tc[0] );
 
   return t.girdle();
 };
