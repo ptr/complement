d50b62b58ef3c8172cbee33e11f181f6776db130
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2008-04-25 08:53:58 +0000

    fix dependency between tests; sample of setting dependency between tests
    git-svn-id: svn://localhost/complement/trunk@1857 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/app/SMTP-tools/smtp_server_ut/my_test_suite.cc
+++ complement/explore/app/SMTP-tools/smtp_server_ut/my_test_suite.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <08/03/26 10:12:21 ptr>
+// -*- C++ -*- Time-stamp: <08/04/25 12:01:55 yeti>
 
 #include "my_test_suite.h"
 #include "my_test.h"
@@ -7,11 +7,20 @@
 
 int EXAM_IMPL(my_test_suite)
 {
-  exam::test_suite t( "my test" );
+  exam::test_suite t( "test suite for SMTP server" );
   my_test test;
 
-  t.add( &my_test::test_gen, test, "my_test::test_gen" );
-  t.add( &my_test::thread_call, test, "my_test::thread_call" );
+  // exam::test_suite::test_case_type tc0;
+
+
+  t.add( &my_test::thread_call, test, "my_test::thread_call",
+    t.add( &my_test::test_gen, test, "my_test::test_gen" ) );
+
+#if 0
+  tc0 = t.add( &my_test::test_gen, test, "my_test::test_gen" );
+  t.add( &my_test::thread_call, test, "my_test::thread_call", tc0 );
+  t.add( &my_test::thread_call, test, "my_test::thread_call_yet_more", tc0 );
+#endif
 
   return t.girdle();
 };
