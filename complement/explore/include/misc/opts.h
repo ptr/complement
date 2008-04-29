3eb11e3c2962c3c7c5d132a8cf6436b635bfc411
DmitryOsmakov <DmitryOsmakov@09cbc127-031f-0410-9985-c3ce8c6d183e>
2008-04-29 09:41:55 +0000

    fixed lib Opts : fixed problem with destructor and changed view on setting default values
    git-svn-id: svn://localhost/complement/trunk@1865 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/include/misc/opts.h
+++ complement/explore/include/misc/opts.h
@@ -10,245 +10,122 @@
 
 using namespace std;
 
-class Value
-{
-public:
-	void* ptr;
-	Value() { ptr = 0; }
-};
-
 class Opt
 {
 public:
-	char shortname;
-	string longname;
-	string desc;
-	vector< string > args;
-	Value  val; // must be vector
+  char shortname;
+  string longname;
+  string desc;
+  vector< string > args;
 
-	bool has_arg;
-	bool is_set;
+  bool has_arg;
+  bool is_set;
 };
 
 class Opts
 {
 public:
 // construct
-	Opts(const string& _brief = "",const string& _author = "",const string& _copyright = "") : brief(_brief) , author(_author) , copyright(_copyright) {};
+  Opts(const string& _brief = "",const string& _author = "",const string& _copyright = "") : brief(_brief) , author(_author) , copyright(_copyright) {};
 
 // adding option 
-	// 4 params
-	template <class T>
-	void add(char _shortname,const string& _longname,const string& _desc,T v);
-	
-	
-	// 3 params
-	template <class T>
-	void add(char _shortname,const string& desc,T v);
-	template <class T>
-	void add(const string& longname,const string& desc,T v);
-	
-	// 2 params
-	template <class T>
-	void add(char _shortname,T v);
-	template <class T>
-	void add(const string& _longname,T v);
-
-	// 1 param - randomly generated names?
-
-// adding flag 
-	void addf(char _shortname,const string& _longname,const string& _desc);
-	void addf(char _shortname,const string& _desc);
-	void addf(const string& _longname,const string& _desc);
+  void add(char _shortname,const string& _longname = "",const string& _desc = "",bool has_arg = false);
 
 // getting option
-	template <class T>
-	T get(char _shortname,T& dest);
-	template <class T>
-	T get(const string& _longname,T& dest);
-	
-	bool is_set(char _shortname);
-	bool is_set(const string& _longname);
+  template <class T>
+  T get(char _shortname,T& dest);
+  template <class T>
+  T get(const string& _longname,T& dest);
+  
+  bool is_set(char _shortname);
+  bool is_set(const string& _longname);
+
 
 // parse
-	void parse(int ac,char** av);
+  void parse(int ac,char** av);
 
 // stuff
-	void help(ostream& out);
-	string get_pname() const;
-	string get_brief() const;
-	string get_author() const;
-	string get_copyright() const;
+  void help(ostream& out);
+  string get_pname() const;
+  string get_brief() const;
+  string get_author() const;
+  string get_copyright() const;
 
 // error handling
-	struct invalid_opt { string optname; invalid_opt(const string& _optname) : optname(_optname) {}; };
-	struct missing_arg { string optname; missing_arg(const string& _optname) : optname(_optname) {}; };
-	struct invalid_arg { string optname,argname; invalid_arg(const string& _optname,const string& _argname) : optname(_optname) , argname(_argname) {}; };
+  struct invalid_opt { string optname; invalid_opt(const string& _optname) : optname(_optname) {}; };
+  struct missing_arg { string optname; missing_arg(const string& _optname) : optname(_optname) {}; };
+  struct invalid_arg { string optname,argname; invalid_arg(const string& _optname,const string& _argname) : optname(_optname) , argname(_argname) {}; };
 
+  
+  vector< string > args;
 private:
-	// data	
-	vector< Opt > storage;	
-	vector< string > args;
-	
-	string pname;
-	string brief;
-	string author;
-	string copyright;
+  // data  
+  vector< Opt > storage;  
+  
+  string pname;
+  string brief;
+  string author;
+  string copyright;
 
-	bool isterm(const string& s);
-	bool is_opt_name(const string& s);
-	bool is_flag_group(const string& s);
-	bool is_substr(const string& small,const string& big);
-	int get_opt_index(string s);
+  bool isterm(const string& s);
+  bool is_opt_name(const string& s);
+  bool is_flag_group(const string& s);
+  bool is_substr(const string& small,const string& big);
+  int get_opt_index(string s);
 };
-
-template <class T>
-void Opts::add(char _shortname,const string& _longname,const string& _desc,T v)
-{
-	Opt opt;
-
-	//opt.val.tinfo = typeid(T);
-	opt.val.ptr = new T;
-	*(reinterpret_cast<T*>(opt.val.ptr)) = v;
-
-	opt.shortname = _shortname;
-	opt.longname = _longname;
-	opt.desc = _desc;
-
-	opt.has_arg = true;
-	opt.is_set = false;
-	
-	storage.push_back(opt);
-}
-
-template <class T>
-void Opts::add(char _shortname,const string& _desc,T v)
-{
-	Opt opt;
-
-	//opt.val.tinfo = typeid(T);
-	opt.val.ptr = new T;
-	*(reinterpret_cast<T*>(opt.val.ptr)) = v;
-
-	opt.shortname = _shortname;
-	opt.desc = _desc;
-
-	opt.has_arg = true;
-	opt.is_set = false;
-	
-	storage.push_back(opt);
-}
-
-template <class T>
-void Opts::add(const string& _longname,const string& _desc,T v)
-{
-	Opt opt;
-
-	//opt.val.tinfo = typeid(T);
-	opt.val.ptr = new T;
-	*(reinterpret_cast<T*>(opt.val.ptr)) = v;
-
-	opt.longname = _longname;
-	opt.desc = _desc;
-
-	opt.has_arg = true;
-	opt.is_set = false;
-	
-	storage.push_back(opt);
-}
-
-template <class T>
-void Opts::add(char _shortname,T v)
-{
-	Opt opt;
-
-	//opt.val.tinfo = typeid(T);
-	opt.val.ptr = new T;
-	*(reinterpret_cast<T*>(opt.val.ptr)) = v;
-
-	opt.shortname = _shortname;
-
-	opt.has_arg = true;
-	opt.is_set = false;
-	
-	storage.push_back(opt);
-}
-
-template <class T>
-void Opts::add(const string& _longname,T v)
-{
-	Opt opt;
-
-	//opt.val.tinfo = typeid(T);
-	opt.val.ptr = new T;
-	*(reinterpret_cast<T*>(opt.val.ptr)) = v;
-
-	opt.longname = _longname;
-
-	opt.has_arg = true;
-	opt.is_set = false;
-	
-	storage.push_back(opt);
-}
 
 template <class T>
 T Opts::get(char _shortname,T& res)
 {
-	int i;
-	for (i = 0;i < storage.size();i++)
-		if (storage[i].shortname == _shortname)
-		{
-			if (storage[i].is_set && storage[i].has_arg)
-			{
-				try
-				{
-					stringstream ss(storage[i].args[0]);
-					ss >> res;
-				}
-				catch(...)
-				{
-					throw invalid_arg(string("-") + string(1,_shortname),storage[i].args[0]);
-				}
-			}
-			else
-			{
-				res = *reinterpret_cast<T*>(storage[i].val.ptr);
-			}
-			break;
-		}	
-	if (i == storage.size())
-		throw invalid_opt(string("-") + string(1,_shortname));
-	return res;
+  int i;
+  for (i = 0;i < storage.size();i++)
+    if (storage[i].shortname == _shortname)
+    {
+      if (storage[i].is_set && storage[i].has_arg)
+        if (!storage[i].args.empty())
+        {
+          try
+          {
+            stringstream ss(storage[i].args[0]);
+            ss >> res;
+          }
+          catch(...)
+          {
+            throw invalid_arg(string("-") + string(1,_shortname),storage[i].args[0]);
+          }
+        }
+      break;
+    }  
+  if (i == storage.size())
+    throw invalid_opt(string("-") + string(1,_shortname));
+  return res;
 }
 
 template <class T>
 T Opts::get(const string& _longname,T& res)
 {
-	int i;
-	for (i = 0;i < storage.size();i++)
-		if (storage[i].longname == _longname)
-		{
-			if (storage[i].is_set && storage[i].has_arg)
-			{
-				try
-				{
-					stringstream ss(storage[i].args[0]);
-					ss >> res;
-				}
-				catch(...)
-				{
-					throw invalid_arg(_longname,storage[i].args[0]);
-				}
-			}
-			else
-			{
-				res = *reinterpret_cast<T*>(storage[i].val.ptr);
-			}
-			break;
-		}	
-	if (i == storage.size())
-		throw invalid_opt(_longname);
-	return res;	
+  int i;
+  for (i = 0;i < storage.size();i++)
+    if (storage[i].longname == _longname)
+    {
+      if (storage[i].is_set && storage[i].has_arg)
+        if  (!storage[i].args.empty())
+        {
+          try
+          {
+            stringstream ss(storage[i].args[0]);
+            ss >> res;
+          }
+          catch(...)
+          {
+            throw invalid_arg(_longname,storage[i].args[0]);
+          }
+        }
+      break;
+    }  
+  if (i == storage.size())
+    throw invalid_opt(_longname);
+  return res;  
 }
 
 #endif
--- complement/explore/lib/misc/opts_usage.cpp
+++ complement/explore/lib/misc/opts_usage.cpp
@@ -6,74 +6,87 @@ using namespace std;
 
 struct point
 {
-	int x;
-	int y;
-	point(int _x = 0,int _y = 0) : x(_x) , y(_y) {};
+  int x;
+  int y;
+  point(int _x = 0,int _y = 0) : x(_x) , y(_y) {};
 };
 
 istream& operator>>(istream& t,point& p)
 {
-	t >> p.x >> p.y;
-	return t;	
+  t >> p.x >> p.y;
+  return t;  
 }
 
 ostream& operator<<(ostream& t,const point& p)
 {
-	t << p.x << ' ' << p.y;
-	return t;
+  t << p.x << ' ' << p.y;
+  return t;
 }
 
 int main(int ac,char** av)
 {
-	Opts opts;	
-	point p;
-	string name;
-	int port;
-	
-	opts.addf('h',"help","display help message");
-	opts.addf('v',"verbose","verbose");
-	opts.addf('j',"just","just do it");
+  Opts opts;
 
-	opts.add('p',"port","port number",80);
-	opts.add('s',"point","start point",point(1,1));
-	opts.add('n',"name","your name",string("maos"));
-	
-	try
-	{
-		opts.parse(ac,av);
-	}
-	catch(Opts::invalid_opt& t)
-	{
-		cout << "Invalid option: " << t.optname << endl;
-		return 1;
-	}
-	catch(Opts::invalid_arg& t)
-	{
-		cout << "Invalid argument: " << t.optname << ' ' << t.argname << endl;
-		return 1;
-	}
-	catch(Opts::missing_arg& t)
-	{
-		cout << "Missing argument: " << t.optname << endl;
-		return 1;
-	}
-	
-	if (opts.is_set('h'))	
-		opts.help(cout);
+  // control variables with default values  
+  point p(1,1);
+  string name = "maos";
+  int port = 80;
+  
+  opts.add('h',"help","display help message");
+  opts.add('v',"verbose","verbose");
+  opts.add('j',"just","just do it");
 
-	if (opts.is_set('v'))
-		cout << "Verbose mode is set" << endl;
-	else
-		cout << "Verbose mode is not set" << endl;
+  opts.add('p',"port","port number",true);
+  opts.add('s',"point","start point",true);
+  opts.add('n',"name","your name",true);
+  opts.add('g');
+  
+  try
+  {
+    opts.parse(ac,av);
+  }
+  catch(Opts::invalid_opt& t)
+  {
+    cout << "Invalid option: " << t.optname << endl;
+    return 1;
+  }
+  catch(Opts::invalid_arg& t)
+  {
+    cout << "Invalid argument: " << t.optname << ' ' << t.argname << endl;
+    return 1;
+  }
+  catch(Opts::missing_arg& t)
+  {
+    cout << "Missing argument: " << t.optname << endl;
+    return 1;
+  }
+  
+  if (opts.is_set('h'))  
+    opts.help(cout);
 
-	if (opts.is_set("just"))
-		cout << "Just do it!" << endl;
-	else
-		cout << "Just don't do it!" << endl;
+  if (opts.is_set('v'))
+    cout << "Verbose mode is set" << endl;
+  else
+    cout << "Verbose mode is not set" << endl;
 
-	cout << "port = " << opts.get('p',port) << endl;
-	cout << "point = " << opts.get('s',p) << endl;
-	cout << "name = " << opts.get('n',name) << endl; 
+  if (opts.is_set('g'))
+    cout << "-g is set" << endl;
+  else
+    cout << "-g is not set" << endl;
 
-	return 0;
+  if (opts.is_set("just"))
+    cout << "Just do it!" << endl;
+  else
+    cout << "Just don't do it!" << endl;
+
+  cout << "port = " << opts.get('p',port) << endl;
+  cout << "point = " << opts.get('s',p) << endl;
+  cout << "name = " << opts.get('n',name) << endl; 
+
+  cout << "operands: " << endl;
+
+  for (int i = 0;i < opts.args.size();i++)
+    cout << opts.args[i] << endl;
+
+  return 0;
 }
