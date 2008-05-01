ad814d83c569452f6e83f06b0a014ef4054979a2
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2008-05-01 11:24:15 +0000

    clean code; remove 'use namespace std' from header; push functionality demonstration/development/debugging into unit tests
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1869 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ complement/explore/lib/misc/ut/opts_test.h
@@ -0,0 +1,35 @@
+// -*- C++ -*- Time-stamp: <08/05/01 15:17:31 ptr>
+
+/*
+ * Copyright (c) 2008
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 3.0
+ *
+ */
+
+#ifndef __OPTS_TEST_H
+#define __OPTS_TEST_H
+
+#define FIT_EXAM
+
+#include <exam/suite.h>
+
+class opts_test
+{
+  public:
+    // implementation
+    int EXAM_DECL(bool_option);
+    int EXAM_DECL(bool_option_long);
+    int EXAM_DECL(int_option);
+    int EXAM_DECL(int_option_long);
+    int EXAM_DECL(bad_option);
+    int EXAM_DECL(bad_argument);
+    int EXAM_DECL(multiple);
+    int EXAM_DECL(compound);
+    int EXAM_DECL(args);
+    int EXAM_DECL(stop);
+    int EXAM_DECL(user_defined);
+};
+
+#endif // __MISC_TEST_H
