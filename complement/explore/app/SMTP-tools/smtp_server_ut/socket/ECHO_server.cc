1a1f7944c7b01bf9ae08ced08efc72ad97a6095d
okechina <okechina@09cbc127-031f-0410-9985-c3ce8c6d183e>
2008-05-26 09:20:46 +0000

    Multi-thread realization of TCP-server. A separate thread is created for each client.
    git-svn-id: svn://localhost/complement/trunk@1883 09cbc127-031f-0410-9985-c3ce8c6d183e

--- complement/explore/app/SMTP-tools/smtp_server_ut/socket/ECHO_server.cc
+++ complement/explore/app/SMTP-tools/smtp_server_ut/socket/ECHO_server.cc
@@ -8,30 +8,42 @@
 #include <stdio.h>
 #include <iostream>
 
+#include <pthread.h>
+
 #include "socket.h"
 
 const int bufsize = 4096;
+char buf[bufsize];
+
+int TCPecho (int fd){
+  int cc;
+  while ( cc = read( fd, buf, sizeof( buf ) ) ) {
+    write( fd, buf, cc );
+  }
+  close(fd);
+  return 0;
+}
 
 int main()
 {
   struct sockaddr_in fsin;
-  
+
   char *service = "3000";
 //  char *transport = "udp";
   char *transport = "tcp";
   int qlen = 0;
-  char buf[bufsize];
-
-  sct::sock s ( service, transport, qlen );
   unsigned int alen;
+
+  pthread_t th;
+  
+  sct::sock s ( service, transport, qlen );
+  
   if (transport == "tcp")
   {
-    alen = sizeof( fsin );
-    int sc = accept( s.getDescr(), (struct sockaddr *)&fsin, &alen);
-      
     while (1){
-      int cc = read( sc, buf, sizeof( buf ) );
-      write( sc, buf, cc );
+      alen = sizeof( fsin );
+      int sc = accept( s.getDescr(), (struct sockaddr *)&fsin, &alen);
+      pthread_create( &th, 0, ( void * (*)( void *))TCPecho, (void *)sc );
     }
   } else {
     while (1){
