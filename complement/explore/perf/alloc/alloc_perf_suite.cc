832cbfcf629c220cbe8afff435d0a78c45c205bd
complement <complement@09cbc127-031f-0410-9985-c3ce8c6d183e>
2008-08-12 14:08:09 +0000

    performance: why systems deffer in performance? check malloc; initial test
    git-svn-id: https://complement.svn.sourceforge.net/svnroot/complement/trunk@1956 09cbc127-031f-0410-9985-c3ce8c6d183e

---
+++ complement/explore/perf/alloc/alloc_perf_suite.cc
@@ -0,0 +1,23 @@
+// -*- C++ -*- Time-stamp: <08/08/12 18:06:06 yeti>
+
+/*
+ * Copyright (c) 2008
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License Version 3.0
+ */
+
+#include "alloc_perf_suite.h"
+
+using namespace std;
+
+int EXAM_IMPL(alloc_test::alloc)
+{
+  for ( int i = 0; i < 10000; ++i ) {
+    if ( malloc( 100 ) == 0 ) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
