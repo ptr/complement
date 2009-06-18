3d0289d3dd1ac78644eb45322cfc3364488bb390
Petr Ovtchenkov <ptr@void-ptr.info>
2009-06-18 12:16:08 +0400

    Fix error message for shm_bad_alloc
    Use internal buffer and copy error message from system_error to it.
    Return internal buffer.

--- explore/lib/mt/shm.cc
+++ explore/lib/mt/shm.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*- Time-stamp: <09/05/08 11:09:47 ptr>
+// -*- C++ -*- Time-stamp: <09/06/18 11:59:58 ptr>
 
 /*
  * Copyright (c) 2006, 2009
@@ -10,8 +10,13 @@
 
 #include <mt/shm.h>
 #include <mt/system_error>
+#include <iostream>
 
 namespace xmt {
+
+using namespace std;
+
+static char _buf[128];
 
 static const std::string rstring( "shared memory" );
 
@@ -26,7 +31,9 @@ const char* shm_bad_alloc::what() const throw()
   if ( err == -3 ) {
     return "shared memory: not enough space left in shared memory segment";
   }
-  return std::system_error( err, std::get_posix_category(), rstring ).what();
+  strncpy( _buf, std::system_error( err, std::get_posix_category(), rstring ).what(), sizeof(_buf) );
+  _buf[sizeof(_buf)-1] = 0; // null term, even if room wasn't enough
+  return _buf;
 }
 
 } // namespace xmt
