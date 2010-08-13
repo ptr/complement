ade2ec0c5392333106c7a90822b1505991ae13e0
Dmitry Osmakov <osmakov@yandex-team.ru>
2010-08-13 18:36:24 +0400

    janus: add performance test
      Test creates n_obj objects in one group,
    each object is in separate thread and
    then each object multicasts n_msg messages.
    
    commit eef1adc228af60592a6b7abedf783d8bd80283df
    Author: Dmitry Osmakov <osmakov@yandex-team.ru>
    Date:   Fri Aug 13 18:33:17 2010 +0400
    
        janus: add perfomance test
    
        Test creates n_obj objects in one group,
        each object is in separate thread and
        then each objects multicasts n_msg messages.

---
+++ explore/lib/janus/perf/janus_perf_suite.cc
@@ -0,0 +1,85 @@
+// -*- C++ -*- Time-stamp: <09/07/31 13:51:25 ptr>
+
+/*
+ *
+ * Copyright (c) 2009
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License version 3.0
+ *
+ */
+
+#include "janus_perf.h"
+
+#include <exam/suite.h>
+#include <iostream>
+
+#include <misc/opts.h>
+
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+  Opts opts;
+
+  opts.description( "test suite for 'xmt' framework" );
+  opts.usage( "[options]" );
+
+  opts << option<void>( "print this help message", 'h', "help" )
+       << option<void>( "list all test cases", 'l', "list" )
+       << option<std::string>( "run tests by number <num list>", 'r', "run" )
+       << option<void>( "print status of tests within test suite", 'v', "verbose" )
+       << option<void>(  "trace checks", 't', "trace" );
+
+  try {
+    opts.parse( argc, argv );
+  }
+  catch (...) {
+    opts.help( std::cerr );
+    return 1;
+  }
+
+  if ( opts.is_set( 'h' ) ) {
+    opts.help( std::cerr );
+    return 0;
+  }
+
+  exam::trivial_time_logger tlogger( std::cout );
+  exam::test_suite t( "janus performance test suite 0", 3 );
+  t.set_logger( &tlogger );
+
+  exam::test_suite::test_case_type tc[30];
+
+  janus_perf p;
+
+  t.add( &janus_perf::group_send_mt<4,1000>, p, "group send mt: 4 users, 1000 msgs each",
+    t.add( &janus_perf::group_send_mt<3,1000>, p, "group send mt: 3 users, 1000 msgs each",
+      t.add( &janus_perf::group_send_mt<2,1000>, p, "group send mt: 2 users, 1000 msgs each",
+        t.add( &janus_perf::group_send_mt<1,1000>, p, "group send mt: 1 users, 1000 msgs each" ) ) ) );
+
+  if ( opts.is_set( 'l' ) ) {
+    t.print_graph( std::cerr );
+    return 0;
+  }
+
+  if ( opts.is_set( 'v' ) ) {
+    t.flags( t.flags() | exam::base_logger::verbose );
+  }
+
+  if ( opts.is_set( 't' ) ) {
+    t.flags( t.flags() | exam::base_logger::trace );
+  }
+
+  if ( opts.is_set( 'r' ) ) {
+    std::stringstream ss( opts.get<std::string>( 'r' ) );
+    int n;
+    while ( ss >> n ) {
+      t.single( n );
+    }
+
+    return 0;
+  }
+
+  return t.girdle();
+}
+
