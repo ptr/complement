1a87a78c6b0835d9173d2ef0c8ec5c1cf825e71c
Petr Ovtchenkov <ptr@void-ptr.info>
2010-11-15 14:21:48 +0300

    core syslog test

---
+++ explore/lib/sockios/ut/syslog_test.h
@@ -0,0 +1,22 @@
+// -*- C++ -*- Time-stamp: <2010-11-10 13:59:56 ptr>
+
+/*
+ *
+ * Copyright (c) 2010
+ * Petr Ovtchenkov
+ *
+ * Licensed under the Academic Free License version 3.0
+ *
+ */
+
+#ifndef __syslog_test_h
+#define __syslog_test_h
+
+#include <exam/suite.h>
+
+struct syslog_test
+{
+    int EXAM_DECL(core_test);
+};
+
+#endif // __syslog_test_h
