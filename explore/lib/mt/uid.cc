3719473308425b382970d5fa0e15109a76686ad5
Petr Ovtchenkov <ptr@void-ptr.info>
2017-08-16 15:10:01 +0300

    mt: read formatted uuid correctly
    Use uuid_parse for parsing uuid.
    
    The last part in uuid c2ee3d09-43b3-466d-b490-db35999a22cf
    (db35999a22cf) is a 12-byte (48-bit) integer, that inconvenient
    to process on non-bigendian machines. Use libuuid from util-linux.

--- explore/lib/mt/uid.cc
+++ explore/lib/mt/uid.cc
@@ -88,94 +88,25 @@ std::istream& operator >>( std::istream& s, xmt::uuid_type& uid )
   std::ios_base::fmtflags f = s.flags( static_cast<std::ios_base::fmtflags>(0) );
 #endif
 
-  if ( (f & std::ios_base::skipws) != 0 ) {
-    s.setf( std::ios_base::skipws );
-  }
-
-  std::istream::sentry __sentry( s ); // skip whitespace
+  std::istream::sentry __sentry( s, (f & std::ios_base::skipws) == 0 ); // skip whitespace
 
   if ( !__sentry ) {
     s.flags( f );
     return s;
   }
 
-  s >> hex >> uid.u.i[0];
-#ifdef _LITTLE_ENDIAN
-  swap( uid.u.b[0], uid.u.b[3] );
-  swap( uid.u.b[1], uid.u.b[2] );
-#endif
+  char b[37];
 
-  char c = ' ';
-
-  s >> c;
-
-  if ( c != '-' ) {
-    s.putback( c );
-    s.setstate( std::ios_base::failbit );
-    s.flags( f );
-    return s;
+  copy_n( istreambuf_iterator<char>(s), 36, b );
+  b[36] = '\0';
+  if ( !s.fail() ) {
+    if ( uuid_parse( b, uid.u.b ) < 0 ) {
+      for (int r = 35; r >= 0; --r ) {
+        s.putback( b[r] );
+      }
+      s.setstate( std::ios_base::failbit );
+    }
   }
-  c = ' ';
-
-  s >> uid.u.s[2];
-#ifdef _LITTLE_ENDIAN
-  swap( uid.u.b[4], uid.u.b[5] );
-#endif
-
-  s >> c;
-
-  if ( c != '-' ) {
-    s.putback( c );
-    s.setstate( std::ios_base::failbit );
-    s.flags( f );
-    return s;
-  }
-  c = ' ';
-
-  s >> uid.u.s[3];
-#ifdef _LITTLE_ENDIAN
-  swap( uid.u.b[6], uid.u.b[7] );
-#endif
-
-  s >> c;
-
-  if ( c != '-' ) {
-    s.putback( c );
-    s.setstate( std::ios_base::failbit );
-    s.flags( f );
-    return s;
-  }
-  c = ' ';
-
-  s >> uid.u.s[4];
-#ifdef _LITTLE_ENDIAN
-  swap( uid.u.b[8], uid.u.b[9] );
-#endif
-
-  s >> c;
-
-  if ( c != '-' ) {
-    s.putback( c );
-    s.setstate( std::ios_base::failbit );
-    s.flags( f );
-    return s;
-  }
-
-  stringstream ss;
-
-  copy_n( istreambuf_iterator<char>(s), 4, ostreambuf_iterator<char>(ss) );
-  ss.put( ' ' );
-  copy_n( istreambuf_iterator<char>(s), 8, ostreambuf_iterator<char>(ss) );
-
-  ss >> hex >> uid.u.s[5];
-#ifdef _LITTLE_ENDIAN
-  swap( uid.u.b[10], uid.u.b[11] );
-#endif
-  ss >> uid.u.i[3];
-#ifdef _LITTLE_ENDIAN
-  swap( uid.u.b[12], uid.u.b[15] );
-  swap( uid.u.b[13], uid.u.b[14] );
-#endif
 
   s.flags( f );
 
